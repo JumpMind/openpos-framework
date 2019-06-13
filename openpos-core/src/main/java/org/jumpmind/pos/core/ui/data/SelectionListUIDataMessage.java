acc312efb4994e34047fb9faa3208223e95405e8
stojankz <kstojanovska@jumpmind.com>
2019-06-13 09:59:25 -0400

    update selectionListUIDataMessage to make it hold just a map of the items and disabled items

--- openpos-core/src/main/java/org/jumpmind/pos/core/ui/data/SelectionListUIDataMessage.java
+++ openpos-core/src/main/java/org/jumpmind/pos/core/ui/data/SelectionListUIDataMessage.java
@@ -1,77 +1,44 @@
 package org.jumpmind.pos.core.ui.data;
 
 import org.jumpmind.pos.core.screen.SelectionListItem;
-import org.jumpmind.pos.core.screen.SelectionMode;
 import org.jumpmind.pos.core.ui.UIDataMessage;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.HashMap;
+import java.util.Map;
 
 public class SelectionListUIDataMessage extends UIDataMessage {
 
-    private List<SelectionListItem> items;
-    private List<SelectionListItem> disabledItems;
-    private int numberItemsPerPage;
-    private int numberTotalItems;
-    private int defaultSelectItemIndex = 0;
-    private SelectionMode selectionMode;
+    private Map<Integer , SelectionListItem> items;
+    private Map<Integer, SelectionListItem> disabledItems;
 
     public SelectionListUIDataMessage() {
         setDataType(UIDataMessageType.SELECTION_LIST_DATA);
     }
 
-    public List<SelectionListItem> getItems() {
+    public Map<Integer, SelectionListItem> getItems() {
         if(items == null) {
-            items = new ArrayList<>();
+            items = new HashMap<>();
         }
         return items;
     }
 
-    public void setItems(List<SelectionListItem> items) {
+    public void setItems(Map<Integer, SelectionListItem> items) {
         this.items = items;
     }
 
-    public List<SelectionListItem> getDisabledItems() {
+    public Map<Integer, SelectionListItem> getDisabledItems() {
         if(disabledItems == null) {
-            setDisabledItems(items.stream().filter(i->!i.isEnabled()).collect(Collectors.toList()));
+            disabledItems = new HashMap<>();
+            for(Integer key: getItems().keySet()) {
+                if(!getItems().get(key).isEnabled()) {
+                    disabledItems.put(key, getItems().get(key));
+                }
+            }
         }
         return disabledItems;
     }
 
-    private void setDisabledItems(List<SelectionListItem> disabledItems) {
+    private void setDisabledItems(Map<Integer, SelectionListItem> disabledItems) {
         this.disabledItems = disabledItems;
-    }
-
-    public int getNumberItemsPerPage() {
-        return numberItemsPerPage;
-    }
-
-    public void setNumberItemsPerPage(int numberItemsPerPage) {
-        this.numberItemsPerPage = numberItemsPerPage;
-    }
-
-    public int getNumberTotalItems() {
-        return numberTotalItems;
-    }
-
-    public void setNumberTotalItems(int numberTotalItems) {
-        this.numberTotalItems = numberTotalItems;
-    }
-
-    public int getDefaultSelectItemIndex() {
-        return defaultSelectItemIndex;
-    }
-
-    public void setDefaultSelectItemIndex(int defaultSelectItemIndex) {
-        this.defaultSelectItemIndex = defaultSelectItemIndex;
-    }
-
-    public SelectionMode getSelectionMode() {
-        return selectionMode;
-    }
-
-    public void setSelectionMode(SelectionMode selectionMode) {
-        this.selectionMode = selectionMode;
     }
 }
