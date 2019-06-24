094a61791da5820773308d25f9d31e64d5a30e61
stojankz <kstojanovska@jumpmind.com>
2019-06-24 15:05:29 -0400

    update location-details component so that it opens location override dialog from client end

--- openpos-client-libs/projects/openpos-client-core-lib/src/lib/shared/components/location-details/location-details.component.ts
+++ openpos-client-libs/projects/openpos-client-core-lib/src/lib/shared/components/location-details/location-details.component.ts
@@ -1,6 +1,9 @@
 import { Component, Input } from '@angular/core';
 import { SessionService } from '../../../core/services/session.service';
 import { IActionItem } from '../../../core/interfaces/action-item.interface';
+import { LocationService } from '../../../core/services/location.service';
+import { MatDialogRef, MatDialog } from '@angular/material';
+import { LocationOverrideDialogComponent } from './location-override-dialog/location-override-dialog.component';
 
 @Component({
     selector: 'app-location-details',
@@ -9,14 +12,41 @@ import { IActionItem } from '../../../core/interfaces/action-item.interface';
 })
 
 export class LocationDetailsComponent {
-    @Input() postalCode: string;
-    @Input() locationButton?: IActionItem;
 
-    constructor(public session: SessionService) { }
+    @Input()
+    locationOverridePrompt: string;
+
+    postalCode: string;
+    private overrideDialogRef: MatDialogRef<LocationOverrideDialogComponent>;
+
+    constructor(public session: SessionService, private locationService: LocationService, private matDialog: MatDialog) {
+        locationService.getPostalCode().subscribe(p => {
+            this.postalCode = p;
+        });
+    }
+
+    hasOverridePrompt(): boolean {
+        return this.locationOverridePrompt !== undefined && this.locationOverridePrompt !== null;
+    }
 
     onClick() {
-        if (this.locationButton) {
-            this.session.onAction(this.locationButton);
+        if (this.hasOverridePrompt()) {
+            this.overrideDialogRef = this.matDialog.open(LocationOverrideDialogComponent, {
+                disableClose: true,
+                width: '50%',
+                height: '50%',
+                data: {
+                    countries: this.locationService.getAvailableCountries(),
+                    overridePrompt: this.locationOverridePrompt,
+                    overrideEnabled: this.locationService.hasManualOverride()
+                }
+            });
+
+            this.overrideDialogRef.afterClosed().subscribe( result => {
+                if (result) {
+                    this.locationService.setLocationData(result);
+                }
+            });
         }
     }
 }
