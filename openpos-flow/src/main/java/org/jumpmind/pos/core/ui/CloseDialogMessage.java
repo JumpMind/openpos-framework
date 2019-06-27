018e4babc1c8bc27a28f5def23a0ce13441dae78
Dan Kaste <dan.kaste@leadingedje.com>
2019-06-27 10:56:48 -0400

    Move resposibility of closing dialogs to server
    The client doesn't know the difference between a 'change in state' and just refreshing the screen so
    we will move this responsibility to the server to determine if we should be closing dialogs and the
    client will only create or update screens

--- openpos-flow/src/main/java/org/jumpmind/pos/core/ui/CloseDialogMessage.java
+++ openpos-flow/src/main/java/org/jumpmind/pos/core/ui/CloseDialogMessage.java
@@ -1,4 +1,11 @@
 package org.jumpmind.pos.core.ui;
 
-public class CloseDialogMessage {
+import org.jumpmind.pos.core.model.MessageType;
+import org.jumpmind.pos.util.model.Message;
+
+public class CloseDialogMessage extends Message {
+
+    public CloseDialogMessage(){
+        setType(MessageType.CloseDialog);
+    }
 }
