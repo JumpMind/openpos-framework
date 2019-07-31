96a2276acf91369b23fe503185c0a0bd9dd7e118
maxwellpettit <maxwell.pettit@gmail.com>
2019-07-31 14:10:48 -0400

    Refactor personalization screens

--- openpos-client-libs/projects/openpos-client-core-lib/src/lib/core/components/personalization/personalization.component.ts
+++ openpos-client-libs/projects/openpos-client-core-lib/src/lib/core/components/personalization/personalization.component.ts
@@ -1,12 +1,11 @@
 import { Logger } from './../../services/logger.service';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/forms';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { IScreen } from '../../../shared/components/dynamic-screen/screen.interface';
 import { PersonalizationService } from '../../services/personalization.service';
 import { PersonalizationResponse } from '../../interfaces/personalization-response.interface';
-import { OverlayContainer } from '@angular/cdk/overlay';
-import { SessionService } from '../../services/session.service';
+import { ClientUrlService } from '../../services/client-url.service';
+import { MatDialog } from '@angular/material';
 
 @Component({
     selector: 'app-personalization',
@@ -14,73 +13,151 @@ import { SessionService } from '../../services/session.service';
 })
 export class PersonalizationComponent implements IScreen, OnInit {
 
+    navigateExternal = false;
+
     firstFormGroup: FormGroup;
     secondFormGroup: FormGroup;
-    response: PersonalizationResponse;
-    checkTimeout: any;
+    lastFormGroup: FormGroup;
+    clientResponse: any;
+    serverResponse: PersonalizationResponse;
+    clientTimeout: any;
+    serverTimeout: any;
 
-    constructor(private log: Logger, private personalization: PersonalizationService, private session: SessionService, 
-                private formBuilder: FormBuilder, private http: HttpClient, overlayContainer: OverlayContainer) {
-        overlayContainer.getContainerElement().classList.add('openpos-default-theme');
+    constructor(
+        private formBuilder: FormBuilder, private clientUrlService: ClientUrlService,
+        private personalizationService: PersonalizationService, private log: Logger,
+        private matDialog: MatDialog
+    ) { }
+
+    show(screen: any): void {
     }
 
     ngOnInit() {
-        this.firstFormGroup = this.formBuilder.group({
-            serverName: ['', [Validators.required]],
-            serverPort: ['', [Validators.required, , Validators.pattern('^[0-9]+$')]],
-            sslEnabled: ['']
-        }, { asyncValidator: this.serverValidator });
-        this.updateSecondFormGroup();
+
+        this.navigateExternal = this.clientUrlService.navigateExternal;
+
+        if (this.navigateExternal && localStorage.getItem('clientUrl')) {
+            this.clientUrlService.renavigate();
+        } else {
+            this.firstFormGroup = this.formBuilder.group({
+                clientName: ['', [Validators.required]],
+                clientPort: ['', [Validators.required, , Validators.pattern('^[0-9]+$')]],
+                appName: ['', [Validators.required]],
+                sslEnabled: ['']
+            }, { asyncValidator: this.clientValidator });
+
+            this.secondFormGroup = this.formBuilder.group({
+                serverName: ['', [Validators.required]],
+                serverPort: ['', [Validators.required, , Validators.pattern('^[0-9]+$')]],
+                sslEnabled: ['']
+            }, { asyncValidator: this.serverValidator });
+
+            this.updateLastFormGroup();
+        }
     }
 
-    updateSecondFormGroup() {
+    updateLastFormGroup() {
         let devicePattern = '[a-zA-Z0-9\-]+';
-        if (this.response && this.response.devicePattern) {
-            devicePattern = this.response.devicePattern;
+        if (this.serverResponse && this.serverResponse.devicePattern) {
+            devicePattern = this.serverResponse.devicePattern;
         }
         const formGroup = {
             deviceId: ['', [Validators.required, , Validators.pattern(devicePattern)]]
         };
 
-        if (this.response) {
+        if (this.serverResponse) {
             const validator = [Validators.required, , Validators.pattern('[a-zA-Z0-9]+')];
-            for (const prop of this.response.parameters) {
+            for (const prop of this.serverResponse.parameters) {
                 formGroup[prop.property] = [prop.defaultValue, validator];
             }
         }
 
-        this.secondFormGroup = this.formBuilder.group(formGroup);
+        this.lastFormGroup = this.formBuilder.group(formGroup);
     }
 
-    show(screen: any): void {
+    public navigate() {
+        const clientName = this.firstFormGroup.get('clientName').value;
+        const clientPort = this.firstFormGroup.get('clientPort').value;
+        const appName = this.firstFormGroup.get('appName').value;
+        const clientSslEnabled = this.firstFormGroup.get('sslEnabled').value;
+
+        const serverName = this.secondFormGroup.get('serverName').value;
+        const serverPort = this.secondFormGroup.get('serverPort').value;
+        const serverSslEnabled = this.secondFormGroup.get('sslEnabled').value;
+
+        const deviceId = this.lastFormGroup.get('deviceId').value;
+
+        const personalizationProperties = new Map<string, string>();
+        if (this.serverResponse && this.serverResponse.parameters) {
+            for (const parameter of this.serverResponse.parameters) {
+                personalizationProperties.set(parameter.property, this.lastFormGroup.get(parameter.property).value);
+            }
+        }
+
+        this.clientUrlService.navigate(clientName, clientPort, appName, clientSslEnabled,
+            serverName, serverPort, serverSslEnabled, deviceId, personalizationProperties);
+    }
+
+    public personalizeLocal() {
+        const personalizationProperties = new Map<string, string>();
+        if (this.serverResponse) {
+            for (const parameter of this.serverResponse.parameters) {
+                personalizationProperties.set(parameter.property, this.lastFormGroup.get(parameter.property).value);
+            }
+        }
+        this.personalizationService.personalize(this.secondFormGroup.get('serverName').value, this.secondFormGroup.get('serverPort').value,
+            this.lastFormGroup.get('deviceId').value, personalizationProperties, this.secondFormGroup.get('sslEnabled').value);
     }
 
     public personalize() {
-        const personalizationProperties = new Map<string, string>();
-        if (this.response) {
-            for (const prop of this.response.parameters) {
-                personalizationProperties.set(prop.property, this.secondFormGroup.get(prop.property).value);
-            }
+        if (this.navigateExternal) {
+            this.navigate();
+        } else {
+            this.personalizeLocal();
+            this.matDialog.closeAll();
         }
-        this.personalization.personalize(this.firstFormGroup.get('serverName').value, this.firstFormGroup.get('serverPort').value,
-            this.secondFormGroup.get('deviceId').value, personalizationProperties, this.firstFormGroup.get('sslEnabled').value);
+    }
+
+    private updateDefaultServerValues(clientName: string) {
+        this.secondFormGroup.get('serverName').setValue(clientName);
+        this.secondFormGroup.get('serverPort').setValue('6140');
+    }
+
+    clientValidator = async (control: AbstractControl) => {
+        clearTimeout(this.clientTimeout);
+        const clientName = control.get('clientName').value;
+        const clientPort = control.get('clientPort').value;
+        const appName = control.get('appName').value;
+        const clientSslEnabled: boolean = control.get('sslEnabled').value;
+
+        return new Promise((resolve, reject) => {
+            this.clientTimeout = setTimeout(async () => {
+                this.clientResponse = await this.clientUrlService.checkClientStatus(clientName, clientPort, appName, clientSslEnabled);
+                if (this.clientResponse.success) {
+                    this.updateDefaultServerValues(clientName);
+                    resolve(null);
+                } else {
+                    resolve(this.clientResponse);
+                }
+            }, 1000);
+        });
     }
 
     serverValidator = async (control: AbstractControl) => {
-        clearTimeout(this.checkTimeout);
+        clearTimeout(this.serverTimeout);
         const serverName = control.get('serverName').value;
         const serverPort = control.get('serverPort').value;
         const sslEnabled: boolean = control.get('sslEnabled').value;
 
         return new Promise((resolve, reject) => {
-            this.checkTimeout = setTimeout(async () => {
-                this.response = await this.session.requestPersonalization({ serverName: serverName, serverPort: serverPort, useSsl: sslEnabled });
-                if (this.response.success) {
-                    this.updateSecondFormGroup();
+            this.serverTimeout = setTimeout(async () => {
+                this.serverResponse = await this.personalizationService.requestPersonalization(serverName, serverPort, sslEnabled);
+                if (this.serverResponse.success) {
+                    this.updateLastFormGroup();
                     resolve(null);
                 } else {
-                    this.log.info(`Personalization request failed with error: ${this.response.message}`);
-                    resolve(this.response);
+                    this.log.warn(`Personalization request failed with error: ${this.serverResponse.message}`);
+                    resolve(this.serverResponse);
                 }
             }, 1000);
         });
--- openpos-client-libs/projects/openpos-client-core-lib/src/lib/version.ts
+++ openpos-client-libs/projects/openpos-client-core-lib/src/lib/version.ts
@@ -1,4 +1,4 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
-export const VERSION = {"componentName":"openpos-client-core-lib","version":"0.3.7-SNAPSHOT","gitHash":"5cb4039de07074a4884de6f761d63d10dfe7f7d5","buildTime":"2019-07-11 21:06:13","gitBranch":"master"};
+export const VERSION = {"componentName":"openpos-client-core-lib","version":"0.5.0-SNAPSHOT","gitHash":"6053e1763d264acf07a163c6a7d6a4e628df9f1e","buildTime":"2019-07-30 19:11:09","gitBranch":"feature/personalize-project"};
 /* tslint:enable */
