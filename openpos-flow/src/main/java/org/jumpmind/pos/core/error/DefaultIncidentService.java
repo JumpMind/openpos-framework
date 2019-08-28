a188f6ce2322a3bcbde9d0d262904f6ffb9a6267
Dan Kaste <dan.kaste@leadingedje.com>
2019-08-28 16:31:15 -0400

    wip

--- openpos-flow/src/main/java/org/jumpmind/pos/core/error/DefaultIncidentService.java
+++ openpos-flow/src/main/java/org/jumpmind/pos/core/error/DefaultIncidentService.java
@@ -1,9 +1,11 @@
 package org.jumpmind.pos.core.error;
 
+import org.jumpmind.pos.core.ui.Toast;
 import org.jumpmind.pos.util.model.Message;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -11,11 +13,15 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Scanner;
 
-@Configurable
+@ConfigurationProperties(prefix = "openpos.incident")
+@Component
 public class DefaultIncidentService implements IIncidentService {
-    private static final Logger log = LoggerFactory.getLogger(DefaultIncidentService.class);
-​
+    private static final Logger logger = LoggerFactory.getLogger(DefaultIncidentService.class);
+
     private static final String INCIDENT_ID_FILENAME = ".incident_id";
+
+    private String incidentIdFileLocation;
+    private String incidentMessage;
 
     private File incidentIdFile;
     private Integer incidentCount;
@@ -23,11 +29,25 @@ public class DefaultIncidentService implements IIncidentService {
 
     @Override
     public Message createIncident(Throwable throwable, IncidentContext incidentContext) {
+        String incidentId = "unknown";
+        try {
+            incidentId = generateIncidentId(incidentContext.getDeviceId());
+        } catch (Exception ex) {
+            logger.warn("Failed to generate incident id", ex);
+        }
 
-        return null;
+        logger.error(
+                String.format("Incident ID %s created for %s",
+                        incidentId,
+                        throwable != null ? throwable.getClass().getSimpleName() : "Unknown Exception"
+                ),
+                throwable
+        );
+
+        return Toast.createWarningToast(String.format(incidentMessage, incidentId));
     }
 
-    private synchronized String generateIncidentId( String deviceId ) {
+    private synchronized String generateIncidentId(String deviceId) {
         int lastId = getLastIncidentCount();
         this.incidentCount = ++lastId;
         persistIncidentCount();
@@ -37,21 +57,22 @@ public class DefaultIncidentService implements IIncidentService {
     }
 
     private void persistIncidentCount() {
-        try(
+        try (
                 PrintWriter printWriter = new PrintWriter(incidentIdFile);
         ) {
             printWriter.print(this.incidentCount != null ? this.incidentCount : 0);
         } catch (IOException e) {
-            log.warn(String.format("Failed to create '%s' file. Reason: %s", INCIDENT_ID_FILENAME, e.getMessage()), e);
+            logger.warn(String.format("Failed to create '%s' file. Reason: %s", INCIDENT_ID_FILENAME, e.getMessage()), e);
         }
 
     }
+
     private void ensureIncidentIdFileExists() {
         if (null == incidentIdFile) {
-            incidentIdFile = new File(StoreProperties.getInstance().getWorkingDir(), INCIDENT_ID_FILENAME);
+            incidentIdFile = new File(incidentIdFileLocation, INCIDENT_ID_FILENAME);
         }
 
-        if (! incidentIdFile.exists()) {
+        if (!incidentIdFile.exists()) {
             persistIncidentCount();
         }
     }
@@ -71,5 +92,20 @@ public class DefaultIncidentService implements IIncidentService {
 
         return this.incidentCount;
     }
-​
+
+    public String getIncidentIdFileLocation() {
+        return incidentIdFileLocation;
+    }
+
+    public void setIncidentIdFileLocation(String incidentIdFileLocation) {
+        this.incidentIdFileLocation = incidentIdFileLocation;
+    }
+
+    public String getIncidentMessage() {
+        return incidentMessage;
+    }
+
+    public void setIncidentMessage(String incidentMessage) {
+        this.incidentMessage = incidentMessage;
+    }
 }
