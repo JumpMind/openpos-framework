10ca0c2323ac73a47f110e5892ef5cddb0b983c0
Eric <eric.amiralian@leadingedje.com>
2019-08-30 15:57:05 -0400

    Baconator handles all BaconStrip properties

--- openpos-flow/src/main/java/org/jumpmind/pos/core/ui/messagepart/builder/Baconator.java
+++ openpos-flow/src/main/java/org/jumpmind/pos/core/ui/messagepart/builder/Baconator.java
@@ -1,5 +1,6 @@
 package org.jumpmind.pos.core.ui.messagepart.builder;
 
+import org.jumpmind.pos.core.ui.ActionItem;
 import org.jumpmind.pos.core.ui.UIMessage;
 import org.jumpmind.pos.core.ui.messagepart.BaconStripPart;
 import org.jumpmind.pos.core.ui.messagepart.MessagePartConstants;
@@ -13,27 +14,42 @@ public class Baconator {
 	}
 	
 	public Baconator setHeaderText(String headerText) {
-		BaconStripPart baconStrip = (BaconStripPart) message.get(MessagePartConstants.BaconStrip);
-		if (baconStrip != null) {
-			baconStrip.setHeaderText(headerText);
-		} else {
-			baconStrip = new BaconStripPart();
-			baconStrip.setHeaderText(headerText);
-			this.message.addMessagePart(MessagePartConstants.BaconStrip, baconStrip);
-		}
+		BaconStripPart baconStrip = getBaconStrip();
+		baconStrip.setHeaderText(headerText);
 		return this;
 	}
 	
 	public Baconator setHeaderIcon(String icon) {
+		BaconStripPart baconStrip = getBaconStrip();
+		baconStrip.setHeaderIcon(icon);
+		return this;
+	}
+	
+	public Baconator setDeviceId(String deviceId) {
+		BaconStripPart baconStrip = getBaconStrip();
+		baconStrip.setDeviceId(deviceId);
+		return this;
+	}
+	
+	public Baconator setOperatorText(String operatorText) {
+		BaconStripPart baconStrip = getBaconStrip();
+		baconStrip.setOperatorText(operatorText);
+		return this;
+	}
+	
+	public Baconator setBackButton(ActionItem backButton) {
+		BaconStripPart baconStrip = getBaconStrip();
+		baconStrip.setBackButton(backButton);
+		return this;
+	}
+	
+	private BaconStripPart getBaconStrip() {
 		BaconStripPart baconStrip = (BaconStripPart) message.get(MessagePartConstants.BaconStrip);
-		if (baconStrip != null) {
-			baconStrip.setHeaderIcon(icon);
-		} else {
+		if (baconStrip == null) {
 			baconStrip = new BaconStripPart();
-			baconStrip.setHeaderIcon(icon);
 			this.message.addMessagePart(MessagePartConstants.BaconStrip, baconStrip);
 		}
-		return this;
+		return baconStrip;
 	}
 	
 }
