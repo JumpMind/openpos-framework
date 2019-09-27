1f35462c761757096f18419f5f22136ab53e2b62
maxwellpettit <maxwell.pettit@gmail.com>
2019-09-27 14:28:02 -0400

    Add better support for custom content and images

--- openpos-flow/src/main/java/org/jumpmind/pos/core/ui/message/SelfCheckoutHomeUIMessage.java
+++ openpos-flow/src/main/java/org/jumpmind/pos/core/ui/message/SelfCheckoutHomeUIMessage.java
@@ -6,8 +6,38 @@ public class SelfCheckoutHomeUIMessage extends UIMessage {
 
     private static final long serialVersionUID = 1L;
 
+    private String backgroundImage;
+
+    private String prompt;
+
+    private String imageUrl;
+
     public SelfCheckoutHomeUIMessage() {
         setId("selfcheckout-home");
         setScreenType(UIMessageType.SELF_CHECKOUT_HOME);
+    }
+
+    public String getBackgroundImage() {
+        return backgroundImage;
+    }
+
+    public void setBackgroundImage(String backgroundImage) {
+        this.backgroundImage = backgroundImage;
+    }
+
+    public String getPrompt() {
+        return prompt;
+    }
+
+    public void setPrompt(String prompt) {
+        this.prompt = prompt;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
     }
 }
