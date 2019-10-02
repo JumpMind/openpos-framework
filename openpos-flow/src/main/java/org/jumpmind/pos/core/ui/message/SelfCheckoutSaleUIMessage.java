5fe6ab14527d115e79c14f031a44737c2d63e4bc
maxwellpettit <maxwell.pettit@gmail.com>
2019-10-02 12:03:10 -0500

    Separate SelfCheckoutSaleUIMessage from SaleUIMessage

--- openpos-flow/src/main/java/org/jumpmind/pos/core/ui/message/SelfCheckoutSaleUIMessage.java
+++ openpos-flow/src/main/java/org/jumpmind/pos/core/ui/message/SelfCheckoutSaleUIMessage.java
@@ -1,15 +1,35 @@
 package org.jumpmind.pos.core.ui.message;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jumpmind.pos.core.model.Total;
 import org.jumpmind.pos.core.ui.ActionItem;
+import org.jumpmind.pos.core.ui.UIMessage;
+import org.jumpmind.pos.core.ui.data.SellItem;
 import org.jumpmind.pos.core.ui.messagepart.SelfCheckoutMenuPart;
 
-public class SelfCheckoutSaleUIMessage extends SaleUIMessage {
+public class SelfCheckoutSaleUIMessage extends UIMessage {
 
     private static final long serialVersionUID = 1L;
 
     private SelfCheckoutMenuPart selfCheckoutMenu = new SelfCheckoutMenuPart();
 
+    private List<SellItem> items = new ArrayList<>();
+
+    private List<Total> totals;
+
+    private Total grandTotal;
+
+    private boolean transactionActive = false;
+
+    private ActionItem checkoutButton;
+
     private ActionItem logoutButton;
+
+    private ActionItem loyaltyButton;
+
+    private ActionItem promoButton;
 
     private SelfCheckoutCustomer customer;
 
@@ -31,12 +51,79 @@ public class SelfCheckoutSaleUIMessage extends SaleUIMessage {
         this.selfCheckoutMenu = selfCheckoutMenu;
     }
 
+    public List<SellItem> getItems() {
+        return items;
+    }
+
+    public void setItems(List<SellItem> items) {
+        this.items = items;
+    }
+
+    public List<Total> getTotals() {
+        return totals;
+    }
+
+    public void setTotals(List<Total> totals) {
+        this.totals = totals;
+    }
+
+    public void addTotal(String name, String amount) {
+        if (totals == null) {
+            totals = new ArrayList<>();
+        }
+        totals.add(new Total(name, amount));
+    }
+
+    public Total getGrandTotal() {
+        return grandTotal;
+    }
+
+    public void setGrandTotal(Total grandTotal) {
+        this.grandTotal = grandTotal;
+    }
+
+    public void setGrandTotal(String name, String amount) {
+        this.grandTotal = new Total(name, amount);
+    }
+
+    public boolean isTransactionActive() {
+        return transactionActive;
+    }
+
+    public void setTransactionActive(boolean transactionActive) {
+        this.transactionActive = transactionActive;
+    }
+
+    public ActionItem getCheckoutButton() {
+        return checkoutButton;
+    }
+
+    public void setCheckoutButton(ActionItem checkoutButton) {
+        this.checkoutButton = checkoutButton;
+    }
+
     public ActionItem getLogoutButton() {
         return logoutButton;
     }
 
     public void setLogoutButton(ActionItem logoutButton) {
         this.logoutButton = logoutButton;
+    }
+
+    public ActionItem getLoyaltyButton() {
+        return loyaltyButton;
+    }
+
+    public void setLoyaltyButton(ActionItem loyaltyButton) {
+        this.loyaltyButton = loyaltyButton;
+    }
+
+    public ActionItem getPromoButton() {
+        return promoButton;
+    }
+
+    public void setPromoButton(ActionItem promoButton) {
+        this.promoButton = promoButton;
     }
 
     public SelfCheckoutCustomer getCustomer() {
