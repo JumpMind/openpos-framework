4f0edc50f048994f51dbf3671b62445f3040908a
maxwellpettit <maxwell.pettit@gmail.com>
2019-10-07 08:49:18 -0500

    Initial conversion of sale screen to new ux

--- openpos-client-libs/projects/openpos-client-core-lib/src/lib/screens-with-parts/sale/sale.interface.ts
+++ openpos-client-libs/projects/openpos-client-core-lib/src/lib/screens-with-parts/sale/sale.interface.ts
@@ -2,6 +2,7 @@
 import { ITotal } from '../../core/interfaces/total.interface';
 import { IAbstractScreen } from '../../core/interfaces/abstract-screen.interface';
 import { IActionItem } from '../../core/actions/action-item.interface';
+import { ISellItem } from '../../core/interfaces/sell-item.interface';
 
 export interface SaleInterface extends IAbstractScreen {
     totals: ITotal[];
@@ -12,4 +13,5 @@ export interface SaleInterface extends IAbstractScreen {
     locationEnabled: boolean;
     locationOverridePrompt: string;
     checkoutButton: IActionItem;
+    items: ISellItem[];
 }
