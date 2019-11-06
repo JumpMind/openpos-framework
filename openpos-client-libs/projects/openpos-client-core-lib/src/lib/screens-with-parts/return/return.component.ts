c128f32da42137e72b0d04fbcbcc8ffeb11204b0
stojankz <kstojanovska@jumpmind.com>
2019-11-06 10:12:28 -0500

    cleanup return.component

--- openpos-client-libs/projects/openpos-client-core-lib/src/lib/screens-with-parts/return/return.component.ts
+++ openpos-client-libs/projects/openpos-client-core-lib/src/lib/screens-with-parts/return/return.component.ts
@@ -1,7 +1,6 @@
-import { MatDialog } from '@angular/material';
-import { Component, ViewChild, AfterViewInit, OnInit, AfterViewChecked, ElementRef, Injector } from '@angular/core';
+import { Component, ViewChild, AfterViewInit, OnInit, AfterViewChecked, ElementRef, Injector, OnDestroy } from '@angular/core';
 import { ObservableMedia } from '@angular/flex-layout';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { map, startWith } from 'rxjs/operators';
 import { NavListComponent } from '../../shared/components/nav-list/nav-list.component';
 import { PosScreen } from '../pos-screen/pos-screen.component';
@@ -12,6 +11,9 @@ import { ISellItem } from '../../core/interfaces/sell-item.interface';
 import { IActionItem } from '../../core/actions/action-item.interface';
 import { ITransactionReceipt } from '../../shared/components/receipt-card/transaction-receipt.interface';
 import { OpenposMediaService, MediaBreakpoints } from '../../core/media/openpos-media.service';
+import { ScannerService } from '../../core/platform-plugins/scanners/scanner.service';
+import { OnBecomingActive } from '../../core/life-cycle-interfaces/becoming-active.interface';
+import { OnLeavingActive } from '../../core/life-cycle-interfaces/leaving-active.interface';
 
 /**
  * @ignore
@@ -24,13 +26,12 @@ import { OpenposMediaService, MediaBreakpoints } from '../../core/media/openpos-
     templateUrl: './return.component.html',
     styleUrls: ['./return.component.scss']
 })
-export class ReturnComponent extends PosScreen<any> implements AfterViewInit, AfterViewChecked, OnInit {
+export class ReturnComponent extends PosScreen<any> implements AfterViewChecked, OnInit, OnDestroy, OnBecomingActive, OnLeavingActive {
 
     isMobile: Observable<boolean>;
 
     @ViewChild('scrollList') private scrollList: ElementRef;
     public size = -1;
-    initialized = false;    // listData: Observable<ISelectableListData<ISellItem>>;
     individualMenuClicked = false;
 
     public overFlowListSize: Observable<number>;
@@ -40,7 +41,10 @@ export class ReturnComponent extends PosScreen<any> implements AfterViewInit, Af
     public receipts: ITransactionReceipt[];
     public removeReceiptAction: IActionItem;
 
-    constructor(private observableMedia: ObservableMedia, protected dialog: MatDialog, injector: Injector,  media: OpenposMediaService) {
+    private scanServiceSubscription: Subscription;
+
+    constructor(private scannerService: ScannerService, protected dialog: MatDialog, injector: Injector,
+                media: OpenposMediaService, private bottomSheet: MatBottomSheet ) {
         super(injector);
         this.isMobile = media.observe(new Map([
             [MediaBreakpoints.MOBILE_PORTRAIT, true],
@@ -59,39 +63,6 @@ export class ReturnComponent extends PosScreen<any> implements AfterViewInit, Af
         this.dialog.closeAll();
     }
 
-    ngOnInit(): void {
-        const sizeMap = new Map([
-            ['xs', 3],
-            ['sm', 3],
-            ['md', 4],
-            ['lg', 5],
-            ['xl', 5]
-        ]);
-
-        let startSize = 3;
-        sizeMap.forEach((size, mqAlias) => {
-            if (this.observableMedia.isActive(mqAlias)) {
-                startSize = size;
-            }
-        });
-        this.overFlowListSize = this.observableMedia.asObservable().pipe(map(
-            change => {
-                return sizeMap.get(change.mqAlias);
-            }
-        ), startWith(startSize));
-    }
-
-    ngAfterViewInit(): void {
-        this.initialized = true;
-    }
-
-    public onReceiptClick(event: any) {
-        if (this.receipts) {
-            const index = this.receipts.indexOf(event);
-            this.doAction('TransactionDetails', index);
-        }
-    }
-
     ngAfterViewChecked() {
         if (this.items && this.size !== this.items.length) {
             this.scrollToBottom();
@@ -99,10 +70,52 @@ export class ReturnComponent extends PosScreen<any> implements AfterViewInit, Af
         }
     }
 
+    ngOnInit(): void {
+        this.scrollToBottom();
+        this.registerScanner();
+    }
+
+    onBecomingActive() {
+        this.registerScanner();
+    }
+
+    onLeavingActive() {
+        this.unregisterScanner();
+    }
+
+    ngOnDestroy(): void {
+        this.unregisterScanner();
+        this.scannerService.stopScanning();
+        super.ngOnDestroy();
+    }
+
     scrollToBottom(): void {
         try {
             this.scrollList.nativeElement.scrollTop = this.scrollList.nativeElement.scrollHeight;
         } catch (err) { }
+    }
+
+    private registerScanner() {
+        if (typeof this.scanServiceSubscription === 'undefined' || this.scanServiceSubscription === null) {
+            this.scanServiceSubscription = this.scannerService.startScanning().subscribe(scanData => {
+                this.doAction({ action: 'Scan' }, scanData);
+            });
+        }
+    }
+
+    private unregisterScanner() {
+        if (this.scanServiceSubscription !== null) {
+            this.scanServiceSubscription.unsubscribe();
+            this.scanServiceSubscription = null;
+        }
+    }
+
+
+    public onReceiptClick(event: any) {
+        if (this.receipts) {
+            const index = this.receipts.indexOf(event);
+            this.doAction('TransactionDetails', index);
+        }
     }
 
 }
