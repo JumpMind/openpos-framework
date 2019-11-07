1dc1cf35e9652deef2ad0df91489755980758d40
maxwellpettit <maxwell.pettit@gmail.com>
2019-11-07 15:20:59 -0600

    Convert tender screen to new format

--- openpos-client-libs/projects/openpos-client-core-lib/src/lib/screens-with-parts/tender/tender.component.ts
+++ openpos-client-libs/projects/openpos-client-core-lib/src/lib/screens-with-parts/tender/tender.component.ts
@@ -21,23 +21,6 @@ export class TenderComponent extends PosScreen<TenderScreenInterface> {
     }
 
     buildScreen() {
-        this.form = this.formBuilder.group(this.screen.form);
-        if ( !!this.screen.tenderTypeActionNames ) {
-            this.screen.tenderTypeActionNames.forEach( actionName => {
-                this.actionService.registerActionPayload(actionName, () => {
-                    if (this.form.valid) {
-                        return this.formBuilder.buildFormPayload(this.form, this.screen.form);
-                    } else {
-                        // Show errors for each of the fields where necessary
-                        Object.keys(this.form.controls).forEach(f => {
-                            const control = this.form.get(f);
-                            control.markAsTouched({ onlySelf: true });
-                        });
-                        throw Error('form is invalid');
-                    }
-                });
-            });
-        }
     }
 
 }
