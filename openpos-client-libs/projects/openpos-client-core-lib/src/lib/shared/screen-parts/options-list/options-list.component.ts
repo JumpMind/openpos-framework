29fdba59b4593c41c7cddfe15be4fa4506c905a5
maxwellpettit <maxwell.pettit@gmail.com>
2019-11-11 11:17:04 -0600

    Resize options list icons responsively

--- openpos-client-libs/projects/openpos-client-core-lib/src/lib/shared/screen-parts/options-list/options-list.component.ts
+++ openpos-client-libs/projects/openpos-client-core-lib/src/lib/shared/screen-parts/options-list/options-list.component.ts
@@ -1,8 +1,10 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, Injector } from '@angular/core';
 import { OptionsListInterface } from './options-list.interface';
 import { ScreenPart } from '../../../shared/decorators/screen-part.decorator';
 import { ScreenPartComponent } from '../../../shared/screen-parts/screen-part';
 import { IActionItem } from '../../../core/actions/action-item.interface';
+import { Observable } from 'rxjs';
+import { OpenposMediaService, MediaBreakpoints } from '../../../core/media/openpos-media.service';
 
 
 @ScreenPart({
@@ -18,9 +20,21 @@ export class OptionsListComponent extends ScreenPartComponent<OptionsListInterfa
     @Output()
     optionClick = new EventEmitter<IActionItem>();
 
-    screenDataUpdated() { }
+    isMobile: Observable<boolean>;
 
-    buildScreen() { }
+    constructor(injector: Injector, mediaService: OpenposMediaService) {
+        super(injector);
+        this.isMobile = mediaService.observe(new Map([
+            [MediaBreakpoints.MOBILE_PORTRAIT, true],
+            [MediaBreakpoints.MOBILE_LANDSCAPE, true],
+            [MediaBreakpoints.TABLET_PORTRAIT, true],
+            [MediaBreakpoints.TABLET_LANDSCAPE, true],
+            [MediaBreakpoints.DESKTOP_PORTRAIT, false],
+            [MediaBreakpoints.DESKTOP_LANDSCAPE, false]
+        ]));
+    }
+
+    screenDataUpdated() { }
 
     onOptionClick(actionItem: IActionItem): void {
         this.optionClick.emit(actionItem);
