eb5422e17654e633cbf39a897c030b03a8739f59
maxwellpettit <maxwell.pettit@gmail.com>
2019-11-14 13:40:07 -0600

    Fix build error

--- openpos-client-libs/projects/openpos-client-core-lib/src/lib/shared/screen-parts/bacon-strip/bacon-strip.interface.ts
+++ openpos-client-libs/projects/openpos-client-core-lib/src/lib/shared/screen-parts/bacon-strip/bacon-strip.interface.ts
@@ -1,5 +1,4 @@
 import { IActionItem } from '../../../core/actions/action-item.interface';
-import { SearchExpandInputInterface } from '../search-expand-input/search-expand-input.interface';
 
 export interface BaconStripInterface {
     deviceId: string;
