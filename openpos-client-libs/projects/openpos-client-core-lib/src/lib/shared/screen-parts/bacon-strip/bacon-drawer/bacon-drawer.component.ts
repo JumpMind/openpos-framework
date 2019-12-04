0a9de7e242c7c265ee0834b7a241605279373caf
stojankz <kstojanovska@jumpmind.com>
2019-12-04 14:54:18 -0500

    update the bacon-drawer to handle selection of options in the drawer using ArrowUp, ArrowDown, and Tab keybind

--- openpos-client-libs/projects/openpos-client-core-lib/src/lib/shared/screen-parts/bacon-strip/bacon-drawer/bacon-drawer.component.ts
+++ openpos-client-libs/projects/openpos-client-core-lib/src/lib/shared/screen-parts/bacon-strip/bacon-drawer/bacon-drawer.component.ts
@@ -1,8 +1,10 @@
-import {Component, EventEmitter, OnDestroy, Output} from '@angular/core';
+import {Component, EventEmitter, OnDestroy, Output, Injector} from '@angular/core';
 import {IActionItem} from '../../../../core/actions/action-item.interface';
 import {ScreenPart} from '../../../decorators/screen-part.decorator';
 import {ScreenPartComponent} from '../../screen-part';
 import {BaconStripInterface} from '../bacon-strip.interface';
+import { KeyPressProvider } from '../../../providers/keypress.provider';
+import { Configuration } from '../../../../configuration/configuration';
 
 @ScreenPart({name: 'baconStrip'})
 @Component({
@@ -10,17 +12,101 @@ import {BaconStripInterface} from '../bacon-strip.interface';
   templateUrl: './bacon-drawer.component.html',
   styleUrls: ['./bacon-drawer.component.scss']
 })
-export class BaconDrawerComponent extends ScreenPartComponent<BaconStripInterface> implements OnDestroy{
+export class BaconDrawerComponent extends ScreenPartComponent<BaconStripInterface> implements OnDestroy {
 
   @Output()
   buttonClicked = new EventEmitter();
 
+  actions: IActionItem[];
+  selectedIndex = -1;
+
+  constructor(injector: Injector, protected keyPresses: KeyPressProvider) {
+    super(injector);
+
+    this.subscriptions.add(
+      this.keyPresses.subscribe( 'ArrowDown', 1, (event: KeyboardEvent) => {
+        // ignore repeats and check configuration
+        if ( event.repeat || event.type !== 'keydown' || !Configuration.enableKeybinds) {
+          return;
+        }
+        if ( event.type === 'keydown') {
+          this.handleArrowKey(event);
+        }
+      })
+    );
+
+    this.subscriptions.add(
+      this.keyPresses.subscribe( 'ArrowUp', 1, (event: KeyboardEvent) => {
+        // ignore repeats and check configuration
+        if ( event.repeat || event.type !== 'keydown' || !Configuration.enableKeybinds) {
+          return;
+        }
+        if ( event.type === 'keydown') {
+          this.handleArrowKey(event);
+        }
+      })
+    );
+
+    this.subscriptions.add(
+      this.keyPresses.subscribe('Tab', 1, (event: KeyboardEvent) => {
+        if (event.repeat || event.type !== 'keydown' || !Configuration.enableKeybinds) {
+          return;
+        }
+        if ( event.type === 'keydown') {
+          event.preventDefault();
+          this.handleArrowKey(event);
+        }
+      })
+    );
+
+    this.subscriptions.add(
+      this.keyPresses.subscribe('Enter', 1, (event: KeyboardEvent) => {
+        if (event.repeat || event.type !== 'keydown' || !Configuration.enableKeybinds) {
+          return;
+        }
+        if ( event.type === 'keydown') {
+          this.selectOption();
+        }
+      })
+    );
+  }
 
   screenDataUpdated() {
+    this.actions = this.screenData.actions;
   }
 
   buttonClick(action: IActionItem ) {
     this.buttonClicked.emit();
     super.doAction(action);
+  }
+
+  selectOption() {
+    if (this.selectedIndex >= 0 && this.selectedIndex < this.actions.length) {
+        this.buttonClick(this.actions[this.selectedIndex]);
+    }
+  }
+
+  handleArrowKey(event: KeyboardEvent) {
+    let direction = 1;
+    if (event.key === 'ArrowDown' || event.key === 'Tab') {
+      direction = 1;
+    } else if (event.key === 'ArrowUp') {
+      direction = -1;
+    } else {
+      return;
+    }
+
+    let newIndex = this.selectedIndex + direction;
+    if (this.selectedIndex === this.actions.length - 1 && event.key === 'Tab') {
+      newIndex = 0;
+    }
+
+    while (newIndex > 0 && newIndex < this.actions.length - 1 && !this.actions[newIndex].enabled) {
+      newIndex = newIndex + direction;
+    }
+
+    if ( this.actions && newIndex >= 0 && newIndex < this.actions.length && this.actions[newIndex]) {
+        this.selectedIndex = newIndex;
+    }
   }
 }
