7288ba7f65e60a78e368a2fb9cba0cd63903c0b7
stojankz <kstojanovska@jumpmind.com>
2019-12-05 14:55:59 -0500

    update option-button to observe that if the elementRef is focused then set focus on the child button instead.

--- openpos-client-libs/projects/openpos-client-core-lib/src/lib/shared/components/option-button/option-button.component.ts
+++ openpos-client-libs/projects/openpos-client-core-lib/src/lib/shared/components/option-button/option-button.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, ViewChild, ElementRef } from '@angular/core';
+import { DomEventManager } from '../../../core/services/dom-event-manager.service';
 
 @Component({
     selector: 'app-option-button',
@@ -7,15 +8,18 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
   })
 export class OptionButtonComponent {
     @Input() disabled = false;
-    @Input() selected = false;
     @Input() inputType: string;
     @Input() optionTitle: string;
     @Input() optionIcon: string;
     @Input() optionSize: string;
     @Output() buttonClick = new EventEmitter();
 
-    constructor() {
+    @ViewChild('button') button;
 
+    constructor(private elementRef: ElementRef, private domEventManager: DomEventManager) {
+      this.domEventManager.createEventObserver(this.elementRef.nativeElement, 'focus').subscribe(() => {
+        this.button.focus();
+      });
     }
 
     clickFn() {
