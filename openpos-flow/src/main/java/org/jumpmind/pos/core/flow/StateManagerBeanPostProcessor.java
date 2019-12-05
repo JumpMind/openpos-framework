0d6fdf92821a732a29ccb7ed02802dd15a3c3ee0
Jason Mihalick <jason@jcraftsmen.com>
2019-12-05 15:03:40 -0500

    Change autowiring behavior to autowire anything, not just beans in ...
    ... device scope.
    
    Henson suggested this change after I ran into issues with autowiring
    translators when fetching them from the spring context.

--- openpos-flow/src/main/java/org/jumpmind/pos/core/flow/StateManagerBeanPostProcessor.java
+++ openpos-flow/src/main/java/org/jumpmind/pos/core/flow/StateManagerBeanPostProcessor.java
@@ -1,10 +1,8 @@
 package org.jumpmind.pos.core.flow;
 
-import org.jumpmind.pos.core.service.spring.DeviceScope;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.BeanPostProcessor;
-import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -13,20 +11,12 @@ public class StateManagerBeanPostProcessor implements BeanPostProcessor {
     @Autowired
     private StateManagerContainer stateManagerContainer;
     
-    @Autowired
-    private ApplicationContext applicationContext;
     
     @Override
     public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
         IStateManager stateManager = stateManagerContainer.getCurrentStateManager();
         if (stateManager != null && stateManager.getInjector().hasInjections(bean)) {
-                
-            if (DeviceScope.isDeviceScope(beanName)) {                
-                stateManager.performInjectionsOnSpringBean(bean);
-            } else if (!(bean instanceof ITransitionStep)){
-                throw new FlowException("Spring bean requests injections via @In but is not a"
-                        + " device scoped bean. This should be changed to a device scoped bean. beanName=" + beanName + " bean=" + bean);
-            }
+            stateManager.performInjectionsOnSpringBean(bean);
         }
         
         return bean;
