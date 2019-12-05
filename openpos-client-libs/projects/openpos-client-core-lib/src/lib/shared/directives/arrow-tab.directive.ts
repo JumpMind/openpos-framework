473f5a3f0e13fd70d8ca7934446a76d5b5de6885
stojankz <kstojanovska@jumpmind.com>
2019-12-05 15:57:00 -0500

    update arrow-tab-item directive to include isDisabled function which checks if disabled input is true or the nativeElement is disabled. Then in arrow-tab directive update to use isDisabled() and fix logic on when to keep incrementing/decrementing even past last or first item. If the new index goes past the range of items, don't change focus.

--- openpos-client-libs/projects/openpos-client-core-lib/src/lib/shared/directives/arrow-tab.directive.ts
+++ openpos-client-libs/projects/openpos-client-core-lib/src/lib/shared/directives/arrow-tab.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ViewChildren, ElementRef, QueryList, OnDestroy, ContentChildren } from '@angular/core';
+import { Directive, QueryList, OnDestroy, ContentChildren } from '@angular/core';
 import { ArrowTabItemDirective } from './arrow-tab-item.directive';
 import { Subscription } from 'rxjs';
 import { KeyPressProvider } from '../providers/keypress.provider';
@@ -7,15 +7,15 @@ import { KeyPressProvider } from '../providers/keypress.provider';
     selector: '[appArrowTab]'
 })
 export class ArrowTabDirective implements OnDestroy {
-    @ContentChildren(ArrowTabItemDirective, {read: ElementRef})
-    buttons: QueryList<ElementRef>;
+    @ContentChildren(ArrowTabItemDirective)
+    buttons: QueryList<ArrowTabItemDirective>;
 
     private _subscription: Subscription;
 
     constructor( keyPresses: KeyPressProvider) {
         this._subscription = keyPresses.subscribe( 'ArrowUp', 1, (event: KeyboardEvent) => {
 
-            if ( event.repeat || event.type !== 'keydown' ) {
+            if ( event.repeat || event.type !== 'keydown') {
                 return;
             }
 
@@ -27,13 +27,12 @@ export class ArrowTabDirective implements OnDestroy {
             }
 
             let newIndex = index - 1;
-            while (newIndex > 0 && this.buttons.toArray()[newIndex].nativeElement.disabled) {
+            while (newIndex >= 0 && this.buttons.toArray()[newIndex].isDisabled()) {
                 newIndex--;
             }
             if ( newIndex >= 0) {
                 this.buttons.toArray()[newIndex].nativeElement.focus();
             }
-
         });
 
         this._subscription.add( keyPresses.subscribe( 'ArrowDown', 1, (event: KeyboardEvent) => {
@@ -49,7 +48,7 @@ export class ArrowTabDirective implements OnDestroy {
             }
 
             let newIndex = index + 1;
-            while (newIndex < this.buttons.length - 1 && this.buttons.toArray()[newIndex].nativeElement.disabled) {
+            while (newIndex < this.buttons.length && this.buttons.toArray()[newIndex].isDisabled()) {
                 newIndex++;
             }
             if ( newIndex < this.buttons.length) {
