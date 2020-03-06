43db883b547373b1457f02324120fd620e1df9e5
Jason Mihalick <jason@jcraftsmen.com>
2020-03-06 13:57:45 -0500

    Fix double handling of events

--- openpos-hazelcast/src/main/java/org/jumpmind/pos/hazelcast/HazelcastPublisher.java
+++ openpos-hazelcast/src/main/java/org/jumpmind/pos/hazelcast/HazelcastPublisher.java
@@ -5,7 +5,6 @@ import com.hazelcast.topic.ITopic;
 import lombok.extern.slf4j.Slf4j;
 import org.jumpmind.pos.util.event.AppEvent;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
@@ -13,7 +12,7 @@ import org.springframework.stereotype.Component;
 @Slf4j
 @Component
 @Profile("hazelcast")
-public class HazelcastPublisher implements ApplicationListener<AppEvent> {
+public class HazelcastPublisher {
 
     @Autowired
     HazelcastInstance hz;
@@ -24,13 +23,13 @@ public class HazelcastPublisher implements ApplicationListener<AppEvent> {
     @EventListener
     public void onApplicationEvent(AppEvent event) {
         if (!event.isRemote()) {
-            log.info("{} received an event {},{} from {}. PUBLISHING IT ", this.getClass().getSimpleName(), event.toString(), event.hashCode(), event.getSource());
+            log.info("{} received an event {},{} from {}. PUBLISHING IT ", this.getClass().getSimpleName(), event.toString(), System.identityHashCode(event), event.getSource());
             // then share it with the world
             deviceStatusMap.update(event);
             ITopic<AppEvent> topic = hz.getTopic("nucommerce/events");
             topic.publish(event);
         } else {
-            log.info("{} received an event {},{} from {}.  It was from a remote node already.  NOT PUBLISHING ", this.getClass().getSimpleName(), event.toString(), event.hashCode(), event.getSource());
+            log.info("{} received an event {},{} from {}.  It was from a remote node already.  NOT PUBLISHING ", this.getClass().getSimpleName(), event.toString(), System.identityHashCode(event), event.getSource());
 
         }
     }
