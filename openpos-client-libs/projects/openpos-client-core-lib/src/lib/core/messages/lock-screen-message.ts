d5d5c9980c5ef9e65c9d596bc42e8d3de6e76320
Dan Kaste <dan.kaste@leadingedje.com>
2020-04-03 22:21:59 -0400

    add lock screen

---
+++ openpos-client-libs/projects/openpos-client-core-lib/src/lib/core/messages/lock-screen-message.ts
@@ -0,0 +1,12 @@
+import {IActionItem} from '../actions/action-item.interface';
+import {OpenposMessage} from './message';
+import {MessageTypes} from './message-types';
+
+export class LockScreenMessage implements OpenposMessage{
+    type = MessageTypes.LOCK_SCREEN;
+    imageUrl: string;
+    userText: string;
+    iconName: string;
+    passwordAction: IActionItem;
+
+}
