4f8bd651d57f6282aabaa38776c2bc2e8777bc98
maxwellpettit <maxwell.pettit@gmail.com>
2020-05-07 15:45:30 -0500

    Simplify session subscribe startup task

--- openpos-client-libs/projects/openpos-client-core-lib/src/lib/core/startup/subscribe-to-session-task.ts
+++ openpos-client-libs/projects/openpos-client-core-lib/src/lib/core/startup/subscribe-to-session-task.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
 import { IStartupTask } from './startup-task.interface';
-import { Observable, Subject, timer, Subscription, from, of, concat } from 'rxjs';
+import { Observable, of, merge, throwError, Subject, concat } from 'rxjs';
 import { SessionService } from '../services/session.service';
 import { Router } from '@angular/router';
 import { StartupTaskData } from './startup-task-data';
 import { StartupTaskNames } from './startup-task-names';
-import { takeUntil, map, filter } from 'rxjs/operators';
+import { map, take, timeoutWith } from 'rxjs/operators';
 import { Configuration } from '../../configuration/configuration';
 import { MessageTypes } from '../messages/message-types';
 
@@ -14,86 +14,34 @@ export class SubscribeToSessionTask implements IStartupTask {
     name = StartupTaskNames.SUBSCRIBE_TO_SESSION;
     order = 600;
 
-    private connectionTimeoutSubscr: Subscription;
-    private startupMessageRecv = false;
-
     constructor(
         protected session: SessionService,
         protected router: Router
     ) { }
 
     execute(data: StartupTaskData): Observable<string> {
-        const messages: string[] = [];
         if (!this.session.connected()) {
 
-            data.route.queryParamMap.keys.forEach(key => {
-                this.session.addQueryParam(key, data.route.queryParamMap.get(key));
-            });
+            const subscribe = Observable.create((messages: Subject<string>) => {
+                data.route.queryParamMap.keys.forEach(key => {
+                    this.session.addQueryParam(key, data.route.queryParamMap.get(key));
+                });
+                this.session.unsubscribe();
+                this.session.subscribe();
+                messages.next('Subscribing to server ...');
+                messages.complete();
+            }) as Observable<string>;
 
-            messages.push(`[StartupService] Subscribing to server ...`);
-            this.session.unsubscribe();
-
-            return Observable.create((result: Subject<string>) => {
-                concat(
-                    concat(
-                        ...messages.map(m => of(m)),
-                        from(this.session.subscribe()).pipe(
-                            map(x => '')
-                        )
-                    ).pipe(filter(x => x !== '')),
-                    this.confirmConnection(Configuration.confirmConnectionTimeoutMillis).pipe(map(success => {
-                        if (success) { return 'Connection established'; }
-                        else { throw new Error(`A connection to the server could not be established.`); }
-                    }))
-                ).subscribe(
-                    x => result.next(x),
-                    err => result.error(err && err.hasOwnProperty('message') ? err.message : err),
-                    () => result.complete()
-                );
-            });
+            return concat(
+                subscribe,
+                this.session.getMessages(MessageTypes.STARTUP).pipe(
+                    timeoutWith(Configuration.confirmConnectionTimeoutMillis, throwError('Timed out waiting for server')),
+                    map(() => 'Successfully connected to server'),
+                    take(1))
+            );
         } else {
             // we shouldn't be coming here if we are already subscribed.  lets do a refresh to get a clean start
             window.location.reload();
         }
-    }
-
-    protected subscribeToStartup(): void {
-        this.startupMessageRecv = false;
-        this.session.getMessages(MessageTypes.STARTUP).subscribe(
-            m => this.handleStartupMessage(m)
-        );
-    }
-
-    handleStartupMessage(message: any) {
-        this.startupMessageRecv = true;
-    }
-
-    protected confirmConnection(maxWaitMillis = 7500): Observable<boolean> {
-        this.subscribeToStartup();
-        return Observable.create((result: Subject<boolean>) => {
-            this.connectionTimeoutSubscr = timer(0, 500).pipe(takeUntil(timer(maxWaitMillis))).subscribe(
-                x => {
-                    if (this.session.connected() && this.startupMessageRecv) {
-                        console.info(`Session connection confirmed`);
-                        this.connectionTimeoutSubscr.unsubscribe();
-                        result.next(true);
-                        result.complete();
-                    } else {
-                        console.info(`Session not connected yet`);
-                    }
-                },
-                (err) => {
-                    this.connectionTimeoutSubscr.unsubscribe();
-                    result.next(false);
-                    result.complete();
-                },
-                () => {
-                    this.connectionTimeoutSubscr.unsubscribe();
-                    console.error(`Timed out waiting for connection to be established`);
-                    result.next(false);
-                    result.complete();
-                }
-            );
-        });
     }
 }
