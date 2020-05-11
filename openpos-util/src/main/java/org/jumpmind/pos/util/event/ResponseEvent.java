fff114fd798041e35d0083bcbe6bc3c1e024361c
chenson42 <chenson42@gmail.com>
2020-05-11 19:00:01 -0400

    Update ResponseEvent.java

--- openpos-util/src/main/java/org/jumpmind/pos/util/event/ResponseEvent.java
+++ openpos-util/src/main/java/org/jumpmind/pos/util/event/ResponseEvent.java
@@ -3,7 +3,6 @@ package org.jumpmind.pos.util.event;
 import lombok.Builder;
 import lombok.Data;
 
-@Builder
 @Data
 public class ResponseEvent<T> extends AppEvent {
 
@@ -11,4 +10,13 @@ public class ResponseEvent<T> extends AppEvent {
     String targetAppId;
     String request;
     T payload;
+
+    @Builder
+    public ResponseEvent(String deviceId, String appId, boolean remote, String targetDeviceId, String targetAppId, String request, T payload) {
+        super(deviceId, appId, remote);
+        this.targetDeviceId = targetDeviceId;
+        this.targetAppId = targetAppId;
+        this.request = request;
+        this.payload = payload;
+    }
 }
