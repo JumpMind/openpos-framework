7cdc55b1ded33c2c785320523d3433cc55d72975
Mark Michalek <mmichalek@jumpmind.com>
2020-05-21 14:10:01 -0400

    Pole display integration

--- openpos-peripheral/src/main/java/org/jumpmind/pos/peripheral/poledisplay/PoleDisplay.java
+++ openpos-peripheral/src/main/java/org/jumpmind/pos/peripheral/poledisplay/PoleDisplay.java
@@ -1,10 +1,12 @@
 package org.jumpmind.pos.peripheral.poledisplay;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.jumpmind.pos.print.IConnectionFactory;
 import org.jumpmind.pos.print.PeripheralConnection;
 import org.jumpmind.pos.print.PrintException;
 import org.jumpmind.pos.util.ClassUtils;
+import org.jumpmind.pos.util.ReflectionException;
 
 import java.util.Map;
 
@@ -24,8 +26,11 @@ public class PoleDisplay {
     public void open(Map<String,Object> settings) {
         this.settings = settings;
         try {
-            this.connectionFactory =
-                    (IConnectionFactory) ClassUtils.loadClass((String) this.settings.get("connectionClass")).newInstance();
+            String className = (String)this.settings.get("connectionClass");
+            if (StringUtils.isEmpty(className)) {
+                throw new ReflectionException("The connectionClass setting is required for the pole display, but was not provided.");
+            }
+            this.connectionFactory = ClassUtils.instantiate(className);
         } catch (Exception ex) {
             throw new PrintException("Failed to create the connection factory for " + getClass().getName(), ex);
         }
