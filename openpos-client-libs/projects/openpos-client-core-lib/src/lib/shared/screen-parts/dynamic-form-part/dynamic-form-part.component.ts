25386176254d5fbdfdcd3a47aecac3d12feb1461
Dan Kaste <dan.kaste@leadingedje.com>
2020-07-08 14:40:24 -0400

    Fix updating form
    When form is rebuilt save off the focused element and then refocus
    Field value changes should not block so that it doesn't interrupt the user

--- openpos-client-libs/projects/openpos-client-core-lib/src/lib/shared/screen-parts/dynamic-form-part/dynamic-form-part.component.ts
+++ openpos-client-libs/projects/openpos-client-core-lib/src/lib/shared/screen-parts/dynamic-form-part/dynamic-form-part.component.ts
@@ -1,5 +1,18 @@
-import {Component, EventEmitter, Injector, Input, Output, QueryList, ViewChild, ViewChildren} from '@angular/core';
+import {
+    AfterContentInit,
+    AfterViewInit,
+    Component,
+    EventEmitter,
+    Injector,
+    Input,
+    Output,
+    QueryList,
+    ViewChild,
+    ViewChildren
+} from '@angular/core';
 import {FormGroup} from '@angular/forms';
+import {timeout} from 'rxjs/operators';
+import {ScannerService} from '../../../core/platform-plugins/scanners/scanner.service';
 import {ScreenPartComponent} from '../screen-part';
 import {FormBuilder} from '../../../core/services/form-builder.service';
 import {DynamicFormFieldComponent} from '../../components/dynamic-form-field/dynamic-form-field.component';
@@ -14,7 +27,7 @@ import {IDynamicFormPartEventArg} from './dynamic-form-part-event-arg.interface'
     templateUrl: './dynamic-form-part.component.html',
     styleUrls: ['./dynamic-form-part.component.scss']
 })
-export class DynamicFormPartComponent extends ScreenPartComponent<IForm> {
+export class DynamicFormPartComponent extends ScreenPartComponent<IForm> implements AfterViewInit{
     @Output() formChanges = new EventEmitter<IDynamicFormPartEventArg>();
     @ViewChildren(DynamicFormFieldComponent) children: QueryList<DynamicFormFieldComponent>;
     @ViewChild('formErrors') formErrors: ShowErrorsComponent;
@@ -23,6 +36,8 @@ export class DynamicFormPartComponent extends ScreenPartComponent<IForm> {
     buttons: IFormElement[];
 
     private _alternateSubmitActions: string[];
+    private  lastFocusedId;
+
 
     @Input() set formName(name: string) {
         this.screenPartName = name;
@@ -37,6 +52,14 @@ export class DynamicFormPartComponent extends ScreenPartComponent<IForm> {
     screenDataUpdated() {
         this.buttons = new Array<IFormElement>();
 
+        // Before we rebuild what element has focus so we can restore
+        if(this.children){
+            let focusedField = this.children.find(item => item.field && item.field.focused);
+            if(focusedField){
+                this.lastFocusedId = focusedField.formField.id;
+            }
+        }
+
         this.form = this.formBuilder.group(this.screenData);
         this.form.valueChanges.subscribe(value => {
             this.formBuilder.buildFormPayload(this.form, this.screenData);
@@ -46,6 +69,7 @@ export class DynamicFormPartComponent extends ScreenPartComponent<IForm> {
             });
         });
 
+
         if (this.screenData && this.screenData.formElements) {
             this.screenData.formElements.forEach(element => {
                 if (element.elementType === 'Button') {
@@ -53,6 +77,16 @@ export class DynamicFormPartComponent extends ScreenPartComponent<IForm> {
                 }
             });
         }
+    }
+
+    ngAfterViewInit(): void {
+        this.children.changes.subscribe( (changes) => {
+            if( this.lastFocusedId ){
+                let elementToFocus = this.children.find(item => item.formField.id === this.lastFocusedId);
+                setTimeout(() => elementToFocus.focus());
+            }
+        });
+
     }
 
     @Input()
@@ -90,7 +124,7 @@ export class DynamicFormPartComponent extends ScreenPartComponent<IForm> {
     onFieldChanged(formElement: IFormElement) {
         if (formElement.valueChangedAction) {
             this.formBuilder.buildFormPayload(this.form, this.screenData);
-            this.doAction(formElement.valueChangedAction, this.screenData);
+            this.doAction( {action:formElement.valueChangedAction, doNotBlockForResponse: true}, this.screenData);
         }
     }
 }
