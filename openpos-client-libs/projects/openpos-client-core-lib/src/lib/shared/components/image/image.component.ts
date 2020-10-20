7ec4ca91b81f69c6c240eb34324ef5b640ebac04
Avramovic, Aleksandar <avramovica@ae.com>
2020-10-20 14:58:49 +0200

    PCOM-191: Update of Image Component Error Logic

--- openpos-client-libs/projects/openpos-client-core-lib/src/lib/shared/components/image/image.component.ts
+++ openpos-client-libs/projects/openpos-client-core-lib/src/lib/shared/components/image/image.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input } from '@angular/core';
 import {ImageService} from '../../../core/services/image.service';
+import {ImageSource} from "./image-source.enum";
 
 /**
  * A wrapper around an `<img>` element that will run the URLs through the image service and swap in an alternate image if the primary results in an error.
@@ -30,35 +31,37 @@ export class ImageComponent {
   altText: string;
 
   /**
-   * Alternate image boolean that shows if alternate image loading failed
+   * Original image source
    */
-  altImageNotLoading : boolean = false;
+  orgImgSrc: string;
 
   /**
-   * Boolean that shows if img src and altImg src failed
+   * Array of image source options
    */
-  noImagesFound: boolean = false;
+  imageSources = [ImageSource.ALT_IMG, ImageSource.DEFAULT_NOT_FOUND_IMG, ImageSource.IMG_FAILED]
 
   constructor( private imageService: ImageService) { }
 
   imageError(image){
-    // throw error in case original src, alt img src and img not found src are failing
-    if(this.noImagesFound){
-      throw Error('Image is not loading')
+    let imgSource = this.imageSources[0];
+    let imgUrl;
+    switch (imgSource) {
+      case ImageSource.ALT_IMG:
+        this.orgImgSrc = image.src;
+        imgUrl = this.imageService.replaceImageUrl(this.altImageUrl);
+        break;
+      case ImageSource.DEFAULT_NOT_FOUND_IMG:
+        imgUrl = this.imageService.replaceImageUrl('');
+        break;
+      case ImageSource.IMG_FAILED:
+          if(this.altImageUrl){
+            throw Error(`Images ${this.orgImgSrc} && ${this.altImageUrl} are not loading`)
+          }
+          throw Error(`Image ${this.orgImgSrc} is not loading`)
     }
 
-    // if there is no altImage Url or altImageUrl failed set altImageURL to '' so it will trigger image-not-found from
-    // config in imageService
-    if(!this.altImageUrl || this.altImageNotLoading ){
-      this.noImagesFound = true;
-      this.altImageUrl = ''
-    }
-
-    let altImage = this.imageService.replaceImageUrl(this.altImageUrl);
-    if( image.src !== altImage ){
-      image.src = altImage;
-    }
-    this.altImageNotLoading = true;
+    image.src = imgUrl;
+    this.imageSources.shift();
   }
 
 }
