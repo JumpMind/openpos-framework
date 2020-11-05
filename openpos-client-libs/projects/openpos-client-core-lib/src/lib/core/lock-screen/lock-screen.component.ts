e3b4a9dc1a4e7292602be2462254f084979235dd
stojankz <kstojanovska@jumpmind.com>
2020-11-05 16:13:49 -0500

    clean up imports

--- openpos-client-libs/projects/openpos-client-core-lib/src/lib/core/lock-screen/lock-screen.component.ts
+++ openpos-client-libs/projects/openpos-client-core-lib/src/lib/core/lock-screen/lock-screen.component.ts
@@ -1,7 +1,6 @@
-import {Component, Inject, Injector, OnDestroy, Optional} from '@angular/core';
-import {Observable, Subject, Subscription} from 'rxjs';
+import {Component, Inject, OnDestroy} from '@angular/core';
+import {Observable, Subject} from 'rxjs';
 import {takeUntil, tap} from 'rxjs/operators';
-import { KeyPressProvider } from '../../shared/providers/keypress.provider';
 import {ActionService} from '../actions/action.service';
 import {LockScreenMessage} from '../messages/lock-screen-message';
 import {LOCK_SCREEN_DATA} from './lock-screen.service';
