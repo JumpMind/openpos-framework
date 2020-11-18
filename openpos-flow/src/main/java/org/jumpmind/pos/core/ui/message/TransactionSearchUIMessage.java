07c38b5d40bd24b3d1afc5f3eb49c22b1932d521
Steve Carley <steven.carley@leadingedje.com>
2020-11-18 15:17:25 -0500

    Initial UI changes

--- openpos-flow/src/main/java/org/jumpmind/pos/core/ui/message/TransactionSearchUIMessage.java
+++ openpos-flow/src/main/java/org/jumpmind/pos/core/ui/message/TransactionSearchUIMessage.java
@@ -3,7 +3,11 @@ package org.jumpmind.pos.core.ui.message;
 import lombok.Data;
 import org.jumpmind.pos.core.model.Form;
 import org.jumpmind.pos.core.ui.ActionItem;
+import org.jumpmind.pos.core.ui.ActionItemGroup;
 import org.jumpmind.pos.core.ui.UIMessage;
+import org.jumpmind.pos.core.ui.data.TransactionSummaryFilter;
+
+import java.util.List;
 
 @Data
 public class TransactionSearchUIMessage extends UIMessage {
@@ -17,6 +21,8 @@ public class TransactionSearchUIMessage extends UIMessage {
     private String transSearchModeIcon;
     private Form searchAllParamsForm;
     private String noResultsMessage;
+    private String filtersLabel;
+    private List<ActionItem> filters;
 
     public TransactionSearchUIMessage() {
         this.setScreenType(UIMessageType.TRANSACTION_SEARCH);
