20aa724f2ffcdc9729dfbbccc85d1ce5d6a1b44f
Kevin DeLorey <Kevin.DeLorey@leadingedje.com>
2021-01-12 15:10:34 -0700

    Initial commit of framework interface to provide peripheral selection throughout the application.

---
+++ openpos-util/src/main/java/org/jumpmind/pos/util/peripheral/IPeripheralDeviceSelector.java
@@ -0,0 +1,28 @@
+package org.jumpmind.pos.util.peripheral;
+
+import java.util.List;
+
+public interface IPeripheralDeviceSelector {
+    /**
+     * Called on implementors in order to provide a handle to a invalidator
+     * object.
+     */
+    void initialize(IInvalidationHandle invalidationHandle);
+
+    /**
+     * Gets the name of the category.
+     */
+    String getCategory();
+
+    /**
+     * Gets the current device selection. Use `null` to indicate that there is
+     * currently no selection.
+     */
+    PeripheralDeviceDescription getCurrentSelection();
+
+    /**
+     * Gets a list of possible selections that this reporter instance could
+     * be in.
+     */
+    List<PeripheralDeviceDescription> getPossibleSelections();
+}
