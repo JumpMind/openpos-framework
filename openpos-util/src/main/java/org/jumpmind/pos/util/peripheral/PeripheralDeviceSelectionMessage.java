fdc08482a675c2ac801f534b97d1ea1172982812
Kevin DeLorey <Kevin.DeLorey@leadingedje.com>
2021-01-13 09:55:09 -0700

    Initial commit of the peripheral selection service.

---
+++ openpos-util/src/main/java/org/jumpmind/pos/util/peripheral/PeripheralDeviceSelectionMessage.java
@@ -0,0 +1,19 @@
+package org.jumpmind.pos.util.peripheral;
+
+import lombok.*;
+import org.jumpmind.pos.util.model.Message;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+@AllArgsConstructor
+@Builder
+public class PeripheralDeviceSelectionMessage extends Message {
+    String category;
+    String id;
+    String displayName;
+
+    @Override
+    public String getType() {
+        return "peripheralDeviceSelection";
+    }
+}
