2d9e6391cf20f716a466343d8329e6caab65de82
jchirico8 <jchirico8@gmail.com>
2021-01-18 08:58:30 -0500

    new watermark component

--- openpos-client-libs/projects/openpos-client-core-lib/src/lib/core/messages/message.ts
+++ openpos-client-libs/projects/openpos-client-core-lib/src/lib/core/messages/message.ts
@@ -1,4 +1,6 @@
 export interface OpenposMessage {
     type: string;
     willUnblock?: boolean;
+    screenMessage?: string;
+    showWatermark?: boolean;
 }
--- openpos-client-libs/projects/openpos-client-core-lib/src/lib/shared/components/dynamic-screen/dynamic-screen.component.ts
+++ openpos-client-libs/projects/openpos-client-core-lib/src/lib/shared/components/dynamic-screen/dynamic-screen.component.ts
@@ -1,7 +1,9 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
-import {ActionService} from '../../../core/actions/action.service';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ActionService } from '../../../core/actions/action.service';
 import { MessageProvider } from '../../providers/message.provider';
-import {ToastContainerDirective, ToastrService} from "ngx-toastr";
+import { ToastContainerDirective, ToastrService } from 'ngx-toastr';
+import { SessionService } from '../../../core/services/session.service';
+import { OpenposMessage } from '../../../core/messages/message';
 
 @Component({
     selector: 'app-dynamic-screen',
@@ -9,13 +11,21 @@ import {ToastContainerDirective, ToastrService} from "ngx-toastr";
     styleUrls: ['./dynamic-screen.component.scss'],
     providers: [MessageProvider, ActionService]
 })
-export class DynamicScreenComponent implements OnInit{
+export class DynamicScreenComponent implements OnInit {
     @ViewChild(ToastContainerDirective)
     toastContainer: ToastContainerDirective;
+    showWatermark = false;
+    watermarkMessage: string;
     constructor(
-                messageProvider: MessageProvider,
-                private toastrService: ToastrService) {
-        messageProvider.setMessageType('Screen');
+        private messageProvider: MessageProvider,
+        private toastrService: ToastrService,
+        private sessionService: SessionService
+    ) {
+        this.messageProvider.setMessageType('Screen');
+        this.sessionService.getMessages('Watermark').subscribe((message: OpenposMessage) => {
+            this.showWatermark = message?.showWatermark;
+            this.watermarkMessage = message?.screenMessage;
+        });
     }
 
     ngOnInit() {
