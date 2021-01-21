2bb0401646e76d2c82d3d7a00f204eec67d82dc2
Kevin DeLorey <Kevin.DeLorey@leadingedje.com>
2021-01-21 08:04:14 -0700

    Cleaned up double quotes.

--- openpos-client-libs/projects/openpos-client-core-lib/src/lib/core/messages/peripheral-device-selection.ts
+++ openpos-client-libs/projects/openpos-client-core-lib/src/lib/core/messages/peripheral-device-selection.ts
@@ -1,5 +1,5 @@
-import { MessageTypes } from "./message-types";
-import { OpenposMessage } from "./message";
+import { MessageTypes } from './message-types';
+import { OpenposMessage } from './message';
 
 export class PeripheralDeviceSelectionMessage implements OpenposMessage {
     type = MessageTypes.PERIPHERAL_DEVICE_SELECTION;
