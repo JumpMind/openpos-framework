5ab74e4e3a35032c060284727e3289bb8e882048
Eddie Legg <eddie.legg@leadingedje.com>
2021-01-22 08:38:48 -0500

    run lint

--- openpos-flow/src/main/java/org/jumpmind/pos/core/ui/messagepart/BaconStripPart.java
+++ openpos-flow/src/main/java/org/jumpmind/pos/core/ui/messagepart/BaconStripPart.java
@@ -1,9 +1,5 @@
 package org.jumpmind.pos.core.ui.messagepart;
 
-import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,6 +7,10 @@ import lombok.NoArgsConstructor;
 import org.jumpmind.pos.core.ui.ActionItem;
 import org.jumpmind.pos.core.ui.IHasBackButton;
 import org.jumpmind.pos.core.ui.IconType;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @Builder
