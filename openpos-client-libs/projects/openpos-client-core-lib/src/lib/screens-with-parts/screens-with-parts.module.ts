43cc2277be569a6cbab5a613ec27c2fafda319a4
Mark Michalek <mmichalek@jumpmind.com>
2021-01-26 12:23:05 -0500

    Back off digital reading component which didn't get used

--- openpos-client-libs/projects/openpos-client-core-lib/src/lib/screens-with-parts/screens-with-parts.module.ts
+++ openpos-client-libs/projects/openpos-client-core-lib/src/lib/screens-with-parts/screens-with-parts.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 import {AutoCompleteAddressDialogComponent} from './auto-complete-address/auto-complete-address-dialog.component';
-import {DigitalReadingComponent} from './digital-reading/digital-reading.component';
 
 import { SelectionListScreenComponent } from './selection-list/selection-list-screen.component';
 import { SelectionListScreenDialogComponent } from './selection-list/selection-list-screen-dialog.component';
@@ -74,7 +73,6 @@ const dialogs = [
     ScanInputDialogComponent,
     AutoCompleteAddressDialogComponent,
     ErrorDialogComponent,
-    DigitalReadingComponent,
     DataTableDialogComponent
 ];
 
