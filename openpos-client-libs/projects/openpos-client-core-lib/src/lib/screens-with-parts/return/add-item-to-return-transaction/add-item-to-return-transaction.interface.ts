6672da4f277ced2f89af38316070495634393239
Anton Pomanytskyi <pomanitska@ae.com>
2021-03-15 22:18:28 +0200

    Added config to allow manual entry of receipt detail. Disabled by default. Correction of remarks.

--- openpos-client-libs/projects/openpos-client-core-lib/src/lib/screens-with-parts/return/add-item-to-return-transaction/add-item-to-return-transaction.interface.ts
+++
@@ -1,8 +0,0 @@
-import {IAbstractScreen} from "../../../core/interfaces/abstract-screen.interface";
-import {IActionItem} from "../../../core/actions/action-item.interface";
-import {FormGroup} from "@angular/forms";
-
-export interface AddItemToReturnTransactionInterface extends IAbstractScreen {
-    addItemButton: IActionItem;
-    form: FormGroup;
-}
