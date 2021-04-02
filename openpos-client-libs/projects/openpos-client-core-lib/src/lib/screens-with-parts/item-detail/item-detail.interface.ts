9f3dabd29d5ac4c99aa0a9ee2590653279457999
Dan Kaste <dan.kaste@leadingedje.com>
2021-04-02 17:26:23 -0400

    Product Option Selection (#1226)
    * start supporting item detail changes
    
    * getting item option selection working
    
    * add themes to option selection
    
    Co-authored-by: Kevin DeLorey <Kevin.DeLorey@leadingedje.com>

--- openpos-client-libs/projects/openpos-client-core-lib/src/lib/screens-with-parts/item-detail/item-detail.interface.ts
+++ openpos-client-libs/projects/openpos-client-core-lib/src/lib/screens-with-parts/item-detail/item-detail.interface.ts
@@ -1,6 +1,8 @@
 import {IActionItem} from '../../core/actions/action-item.interface';
 import { IAbstractScreen } from '../../core/interfaces/abstract-screen.interface';
 import { DisplayProperty } from '../../shared/components/display-property/display-property.interface';
+import {BuddyStoreInterface} from './buddy-store.interface';
+import {ProductOptionInterface} from './product-option.interface';
 import {IPromotionInterface} from './promotion.interface';
 
 export interface ItemDetailInterface extends IAbstractScreen {
@@ -15,4 +17,13 @@ export interface ItemDetailInterface extends IAbstractScreen {
     itemNoPromotionsTitle: string;
     promotions: IPromotionInterface[];
     promotionStackingDisclaimer: string;
+    
+    productOptionsComponents: ProductOptionInterface[];
+    onHandLabel: string;
+    onHandCount: string;
+    buddyStoreOffline: boolean;
+    buddyStoreTitle: string;
+    buddyStoreIcon: string;
+    buddyStoreOfflineTitle: string;
+    buddyStores: BuddyStoreInterface[];
 }
