835f559437644895a63f934e319fe34d759bbbb2
Laslo Botos <lbotos@griddynamics.com>
2021-04-09 13:20:54 +0200

    Replaced getters and setters with lombok annotations

--- openpos-flow/src/main/java/org/jumpmind/pos/core/ui/data/TransactionReceipt.java
+++ openpos-flow/src/main/java/org/jumpmind/pos/core/ui/data/TransactionReceipt.java
@@ -1,11 +1,15 @@
 package org.jumpmind.pos.core.ui.data;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.jumpmind.pos.core.model.Total;
 
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+@Getter
+@Setter
 public class TransactionReceipt implements Serializable {
 
     private static final long serialVersionUID = 1L;
@@ -14,9 +18,9 @@ public class TransactionReceipt implements Serializable {
 
     private String webOrderId;
 
-    private List<TransactionReceiptLine> transactionInfoSection;
+    private List<TransactionReceiptLine> transactionInfoSection = new ArrayList<>();
 
-    private List<TransactionReceiptLine> totalsInfoSection;
+    private List<TransactionReceiptLine> totalsInfoSection = new ArrayList<>();
 
     private Total transactionTotal;
 
@@ -24,73 +28,11 @@ public class TransactionReceipt implements Serializable {
 
     private String barcode;
 
-    public String getBarcode() {
-        return barcode;
-    }
-
-    public void setBarcode(String barcode) {
-        this.barcode = barcode;
-    }
-
-    public long getTransactionNumber() {
-        return transactionNumber;
-    }
-
-    public void setTransactionNumber(long transactionNumber) {
-        this.transactionNumber = transactionNumber;
-    }
-
-    public String getWebOrderId() {
-        return webOrderId;
-    }
-
-    public void setWebOrderId(String webOrderId) {
-        this.webOrderId = webOrderId;
-    }
-
-    public List getTransactionInfoSection() {
-        return transactionInfoSection;
-    }
-
-    public void setTransactionInfoSection(List<TransactionReceiptLine> transactionInfoSection) {
-        this.transactionInfoSection = transactionInfoSection;
-    }
-
     public void addTransactionInfoLine(TransactionReceiptLine line) {
-        if(this.transactionInfoSection == null) {
-            this.transactionInfoSection = new ArrayList<>();
-        }
         this.transactionInfoSection.add(line);
     }
 
-    public List getTotalsInfoSection() {
-        return totalsInfoSection;
-    }
-
-    public void setTotalsInfoSection(List<TransactionReceiptLine> totalsInfoSection) {
-        this.totalsInfoSection = totalsInfoSection;
-    }
-
     public void addTotalsInfoLine(TransactionReceiptLine line) {
-        if(this.totalsInfoSection == null) {
-            this.totalsInfoSection = new ArrayList<>();
-        }
         this.totalsInfoSection.add(line);
-    }
-
-    public Total getTransactionTotal() {
-        return transactionTotal;
-    }
-
-    public void setTransactionTotal(Total transactionTotal) {
-        this.transactionTotal = transactionTotal;
-    }
-
-    public String getIcon() {
-        return icon;
-    }
-
-    public void setIcon(String icon) {
-        this.icon = icon;
     }
 }
