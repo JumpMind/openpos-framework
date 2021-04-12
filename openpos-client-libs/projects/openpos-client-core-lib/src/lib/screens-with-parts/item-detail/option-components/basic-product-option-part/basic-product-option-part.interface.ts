2c22cd35fd0dfece1b91e9dc93a62b404667dda1
Dan Kaste <dan.kaste@leadingedje.com>
2021-04-12 09:17:31 -0400

    add option not available ui

--- openpos-client-libs/projects/openpos-client-core-lib/src/lib/screens-with-parts/item-detail/option-components/basic-product-option-part/basic-product-option-part.interface.ts
+++ openpos-client-libs/projects/openpos-client-core-lib/src/lib/screens-with-parts/item-detail/option-components/basic-product-option-part/basic-product-option-part.interface.ts
@@ -1,9 +1,10 @@
 import {IActionItem} from '../../../../core/actions/action-item.interface';
+import {BasicOptionInterface} from './basic-option.interface';
 
 export interface BasicProductOptionPartInterface {
     optionName: string;
     optionPlaceholder: string;
-    options: Map<string, string>;
+    options: BasicOptionInterface[];
     selectOptionAction: IActionItem;
     selectedOption: string;
 }
