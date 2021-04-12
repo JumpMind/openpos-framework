74ecb37493cf69d829f141ca1935bd666b7e18a8
Kevin DeLorey <kevin.delorey@leadingedje.com>
2021-04-12 06:56:07 -0600

    [ISR] Buddy Store UI components. (#1255)

--- openpos-flow/src/main/java/org/jumpmind/pos/core/ui/data/BuddyStore.java
+++ openpos-flow/src/main/java/org/jumpmind/pos/core/ui/data/BuddyStore.java
@@ -1,8 +1,14 @@
 package org.jumpmind.pos.core.ui.data;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class BuddyStore {
     private String storeName;
     private String storeCount;
