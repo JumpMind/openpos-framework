fa3932931e7c6ffb58d33ba4f826613a0089fad7
Steve Carley <steven.carley@leadingedje.com>
2021-04-12 15:23:16 -0400

    Organized imports

--- openpos-util/src/main/java/org/jumpmind/pos/util/web/ConfiguredRestTemplate.java
+++ openpos-util/src/main/java/org/jumpmind/pos/util/web/ConfiguredRestTemplate.java
@@ -21,7 +21,10 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.security.cert.X509Certificate;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Slf4j
 public class ConfiguredRestTemplate extends RestTemplate {
