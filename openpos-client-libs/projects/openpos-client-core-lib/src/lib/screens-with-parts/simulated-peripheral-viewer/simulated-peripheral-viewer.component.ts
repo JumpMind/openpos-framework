d63cd901013a7720b764f4f89d9b240f9cd91238
Mikhail Levshunov <levshunovm@ae.com>
2021-05-17 21:22:09 +0400

    Make it possible to show 2 receipts in the simulator at the same time

--- openpos-client-libs/projects/openpos-client-core-lib/src/lib/screens-with-parts/simulated-peripheral-viewer/simulated-peripheral-viewer.component.ts
+++ openpos-client-libs/projects/openpos-client-core-lib/src/lib/screens-with-parts/simulated-peripheral-viewer/simulated-peripheral-viewer.component.ts
@@ -18,6 +18,7 @@ import {DiscoveryService} from "../../core/discovery/discovery.service";
 export class SimulatedPeripheralViewerComponent extends PosScreen<SimulatedPeripheralInterface> implements OnInit {
 
     receiptUrl: SafeResourceUrl;
+    secondReceiptUrl: SafeResourceUrl;
 
     constructor(injector: Injector, private simulatedPeripheralService : SimulatedPeripheralService,
                 private domSanitizer : DomSanitizer, private personalizationService : PersonalizationService,
@@ -29,10 +30,23 @@ export class SimulatedPeripheralViewerComponent extends PosScreen<SimulatedPerip
         this.subscriptions.add(this.simulatedPeripheralService.getReceiptData().subscribe(m => this.updateReceiptUrl(m)));
     }
 
-    updateReceiptUrl(deviceToken : string) {
+    updateReceiptUrl(deviceToken : string)
+    updateReceiptUrl(deviceToken : Array<string>)
+    updateReceiptUrl(deviceToken : any) {
         if (deviceToken) {
-            const url = `${this.discoveryService.getServerBaseURL()}/document/previewDocument/${deviceToken}`;
-            this.receiptUrl = this.domSanitizer.bypassSecurityTrustResourceUrl(url);
+            if (Array.isArray(deviceToken)) {
+                if (deviceToken.length == 2 && deviceToken[0] && deviceToken[1]) {
+                    const firstUrl = `${this.discoveryService.getServerBaseURL()}/document/previewDocument/${deviceToken[0]}`;
+                    this.receiptUrl = this.domSanitizer.bypassSecurityTrustResourceUrl(firstUrl);
+
+                    const secondUrl = `${this.discoveryService.getServerBaseURL()}/document/previewDocument/${deviceToken[1]}`;
+                    this.secondReceiptUrl = this.domSanitizer.bypassSecurityTrustResourceUrl(secondUrl);
+                }
+            } else {
+                const url = `${this.discoveryService.getServerBaseURL()}/document/previewDocument/${deviceToken}`;
+                this.receiptUrl = this.domSanitizer.bypassSecurityTrustResourceUrl(url);
+                this.secondReceiptUrl = null;
+            }
         }
     }
 
