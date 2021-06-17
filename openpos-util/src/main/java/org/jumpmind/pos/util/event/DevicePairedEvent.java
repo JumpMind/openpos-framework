fc6433c04de876d4b8feb04fdb075721bc992775
Andy Blubaugh <blubaugha@users.noreply.github.com>
2021-06-17 11:15:36 -0400

    FIXED GI JOE'S BUILD ERROR!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- openpos-util/src/main/java/org/jumpmind/pos/util/event/DevicePairedEvent.java
+++ openpos-util/src/main/java/org/jumpmind/pos/util/event/DevicePairedEvent.java
@@ -1,12 +1,21 @@
 package org.jumpmind.pos.util.event;
 
+import lombok.Data;
 import lombok.ToString;
 
 @ToString(callSuper = true)
+@Data
 public class DevicePairedEvent extends AppEvent {
     private static final long serialVersionUID = 1L;
 
+    private String currentUsername;
+
     public DevicePairedEvent(String deviceId, String appId, String pairedDeviceId) {
         super(deviceId, appId, pairedDeviceId);
+    }
+
+    public DevicePairedEvent(String deviceId, String appId, String pairedDeviceId, String currentUsername) {
+        super(deviceId, appId, pairedDeviceId);
+        this.currentUsername = currentUsername;
     }
 }
