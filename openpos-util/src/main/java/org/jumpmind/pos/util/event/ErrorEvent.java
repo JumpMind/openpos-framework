37841bbf6a16acc35bb54815641555365f7fac3a
Andy Blubaugh <blubaugha@users.noreply.github.com>
2021-06-18 14:17:58 -0400

    Fixed exception not being set on ErrorEvent

--- openpos-util/src/main/java/org/jumpmind/pos/util/event/ErrorEvent.java
+++ openpos-util/src/main/java/org/jumpmind/pos/util/event/ErrorEvent.java
@@ -6,6 +6,8 @@ import lombok.ToString;
 @ToString(callSuper = true)
 @Data
 public class ErrorEvent extends AppEvent {
+    private static final long serialVersionUID = 1L;
+
     private Throwable throwable;
 
     public ErrorEvent(String deviceId, String appId, String pairedDeviceId, Throwable throwable) {
