75e7c0e8320a169f871e0c85c102a48f1e6de27e
liambell-le <69153515+liambell-le@users.noreply.github.com>
2021-06-24 14:52:26 -0400

    Fix merge issues

--- openpos-client-libs/projects/openpos-client-core-lib/src/lib/shared/screen-parts/customer-information/customer-information.interface.ts
+++ openpos-client-libs/projects/openpos-client-core-lib/src/lib/shared/screen-parts/customer-information/customer-information.interface.ts
@@ -1,14 +1,12 @@
 import {Membership} from '../membership-display/memebership-display.interface';
 import {Reward} from '../rewards-line-item/rewards-line-item.interface';
 import {RewardHistory} from '../rewards-history-line-item/rewards-history-line-item.interface';
-import { IActionItem } from '../../../core/actions/action-item.interface';
 
 export interface CustomerDetails {
     name: string,
     loyaltyNumber: string,
     phoneNumber: string,
     email: string,
-    birthDate: string,
     address: {
         line1: string,
         line2: string,
@@ -18,8 +16,7 @@ export interface CustomerDetails {
     },
     memberships: Membership[],
     rewards: Reward[],
-    rewardHistory: RewardHistory[],
-    itemHistory: PurchasedItem[]
+    rewardHistory: RewardHistory[]
 };
 
 export interface CustomerInformationComponentInterface {
@@ -27,30 +24,4 @@ export interface CustomerInformationComponentInterface {
     phoneIcon: string;
     loyaltyNumberIcon: string;
     locationIcon: string;
-    birthDateIcon: string;
-}
-
-export interface PurchasedItem {
-    title: string;
-    salePrice: string;
-    originalPrice: string;
-    imageUrl: string;
-    labels: UILabel[];
-    transaction: TransactionIdentifier;
-    transactionDetailsAction: IActionItem;
-    itemId: String;
-    itemDetailsAction: IActionItem;
-}
-
-export interface TransactionIdentifier {
-    sequenceNumber: number;
-    deviceId: string;
-    businessDate: string;
-
-    voidedSequenceNumber?: number;
-}
-
-export interface UILabel {
-    icon: string;
-    text: string;
 }
