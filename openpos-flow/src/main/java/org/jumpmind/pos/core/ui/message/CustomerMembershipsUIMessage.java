0ebd92d58b4663b05f0eb07fa9e647cf5919663c
Eddie Legg <74971030+eddielegg-le@users.noreply.github.com>
2021-06-28 15:17:50 -0400

    Classes for membership details state (#1431)

---
+++ openpos-flow/src/main/java/org/jumpmind/pos/core/ui/message/CustomerMembershipsUIMessage.java
@@ -0,0 +1,17 @@
+package org.jumpmind.pos.core.ui.message;
+
+import lombok.Data;
+import org.jumpmind.pos.core.ui.AssignKeyBindings;
+import org.jumpmind.pos.core.ui.UIMessage;
+
+import java.util.List;
+
+@Data
+@AssignKeyBindings
+public class CustomerMembershipsUIMessage extends UIMessage {
+    private static final long serialVersionUID = 1L;
+
+    private String title;
+    private UICustomerSubscriptionsItem customer;
+    List<UISubscriptionAccount> subscriptionAccounts;
+}
