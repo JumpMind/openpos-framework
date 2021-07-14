52cbdfcc20c4a35e8a7f611ab2bef138cf31f301
Brandon Lewis <blewis@manifestcorp.com>
2021-07-14 11:40:47 -0400

    Updated MembershipPlanDetailsDialog and PlanDetailsDisplay component tests

--- openpos-client-libs/projects/openpos-client-core-lib/src/lib/shared/screen-parts/plan-details-display/plan-details-display.component.spec.ts
+++ openpos-client-libs/projects/openpos-client-core-lib/src/lib/shared/screen-parts/plan-details-display/plan-details-display.component.spec.ts
@@ -1,111 +1,90 @@
+import {validateDoesNotExist, validateExist, validateIcon, validateText} from "../../../utilites/test-utils";
+import {ComponentFixture, TestBed} from "@angular/core/testing";
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {MatDialog} from "@angular/material";
+import {ActionService} from "../../../core/actions/action.service";
+import {ElectronService} from "ngx-electron";
+import {CLIENTCONTEXT} from '../../../core/client-context/client-context-provider.interface';
+import {TimeZoneContext} from "../../../core/client-context/time-zone-context";
+import {NO_ERRORS_SCHEMA} from "@angular/core";
+import {PlanDetailsDisplayComponent} from "./plan-details-display.component";
+import {Plan} from "../../../screens-with-parts/sale/program-interface";
+import {KeyPressProvider} from "../../providers/keypress.provider";
+import {Subscription} from "rxjs";
+import {ActionItem} from "../../../core/actions/action-item";
 
-
-// class MockActionService {};
-// class MockMatDialog {};
-// class MockElectronService {};
-// class ClientContext {};
-// class MockKeyPressProvider {
-//     subscribe(): Subscription {
-//         return new Subscription();
-//     }
-// };
+class MockKeyPressProvider {
+    subscribe(): Subscription {
+        return new Subscription();
+    }
+};
+class MockActionService {};
+class MockMatDialog {};
+class MockElectronService {};
+class ClientContext {};
 
 describe('PlanDetailsDisplayComponent', () => {
-    // let component: MembershipDisplayComponent;
-    // let fixture: ComponentFixture<MembershipDisplayComponent>;
-    // let membership: Membership;
-    // beforeEach( () => {
-    //     TestBed.configureTestingModule({
-    //         imports: [ HttpClientTestingModule ],
-    //         declarations: [
-    //             MembershipDisplayComponent
-    //         ],
-    //         providers: [
-    //             { provide: MatDialog, useClass: MockMatDialog },
-    //             { provide: ActionService, useClass: MockActionService },
-    //             { provide: ElectronService, useClass: MockElectronService },
-    //             { provide: KeyPressProvider, useClass: MockKeyPressProvider },
-    //             { provide: ClientContext, useValue: {}},
-    //             { provide: CLIENTCONTEXT, useClass: TimeZoneContext}
-    //         ],
-    //         schemas: [
-    //             NO_ERRORS_SCHEMA,
-    //         ]
-    //     }).compileComponents();
-    //     fixture = TestBed.createComponent(MembershipDisplayComponent);
-    //     component = fixture.componentInstance;
-    //     component.screenData = {
-    //         memberIcon: 'check',
-    //         nonMemberIcon: 'close'
-    //     };
-    //     membership = {
-    //         id: '1',
-    //         name: 'My Membership',
-    //         member: true
-    //     };
-    //     component.membership = membership
-    //     fixture.detectChanges();
-    // });
-    //
-    // it('renders', () => {
-    //     expect(component).toBeDefined();
-    // });
-    //
-    // describe('component', () => {
-    // });
-    //
-    // describe('template', () => {
-    //     it('shows the name of the membership', () => {
-    //         validateText(fixture, 'mat-chip', component.membership.name);
-    //     });
-    //
-    //     it('calls clickEvent.emit when the chip is clicked', () => {
-    //         spyOn(component.clickEvent, 'emit');
-    //
-    //         const chip = fixture.debugElement.query(By.css('mat-chip'));
-    //         chip.nativeElement.click();
-    //
-    //         expect(component.clickEvent.emit).toHaveBeenCalledWith(component.membership);
-    //     })
-    //
-    //     describe('when the user is a member', () => {
-    //         beforeEach(() => {
-    //             component.membership.member = true;
-    //             fixture.detectChanges();
-    //         });
-    //
-    //         it('has the "in" class', () => {
-    //             const chip = fixture.debugElement.query(By.css('mat-chip'));
-    //             expect(chip.nativeElement.classList).toContain('in');
-    //         });
-    //
-    //         it('displays the membership icon', () => {
-    //             validateIcon(fixture, 'mat-chip app-icon', 'check');
-    //         });
-    //
-    //         it('does not have the non membership icon', () => {
-    //             validateDoesNotExist(fixture, 'mat-chip app-icon .not-in');
-    //         });
-    //     });
-    //
-    //     describe('when the user is not a member', () => {
-    //         beforeEach(() => {
-    //             component.membership.member = false
-    //             fixture.detectChanges();
-    //         });
-    //
-    //         it('has the "not-in" class', () => {
-    //             const chip = fixture.debugElement.query(By.css('mat-chip'));
-    //             expect(chip.nativeElement.classList).toContain('not-in');
-    //         });
-    //
-    //         it('does not have the membership icon', () => {
-    //             validateDoesNotExist(fixture, 'mat-chip app-icon .in');
-    //         });
-    //
-    //         it('has the non membership icon', () => {
-    //             validateIcon(fixture, 'mat-chip app-icon', 'close');
-    //         });
-    //     });
-    // });
+    let component: PlanDetailsDisplayComponent;
+    let fixture: ComponentFixture<PlanDetailsDisplayComponent>;
+
+    beforeEach( () => {
+        TestBed.configureTestingModule({
+            imports: [ HttpClientTestingModule ],
+            declarations: [
+                PlanDetailsDisplayComponent
+            ],
+            providers: [
+                { provide: MatDialog, useClass: MockMatDialog },
+                { provide: ActionService, useClass: MockActionService },
+                { provide: ElectronService, useClass: MockElectronService },
+                { provide: KeyPressProvider, useClass: MockKeyPressProvider },
+                { provide: ClientContext, useValue: {}},
+                { provide: CLIENTCONTEXT, useClass: TimeZoneContext}
+            ],
+            schemas: [
+                NO_ERRORS_SCHEMA,
+            ]
+        }).compileComponents();
+        fixture = TestBed.createComponent(PlanDetailsDisplayComponent);
+        component = fixture.componentInstance;
+        component.plan = {
+            title: 'Silver Tier',
+            iconImageUrl: 'code',
+            copy: '<b>SILVER TIER</b><hr>Get the good stuff without the extra fancy fluff.',
+            signupActionItem: { title: 'Become A Silver Member', icon: 'add' } as ActionItem
+        } as Plan;
+        fixture.detectChanges();
+        component.screenDataUpdated();
+    });
+
+    it('renders', () => {
+        expect(component).toBeDefined();
+    });
+
+    describe('template', () => {
+        beforeEach(() => {
+            fixture.detectChanges();
+        });
+
+        it('shows the plan title icon', () => {
+            validateExist(fixture, 'app-icon');
+        });
+
+        it('shows the plan title', () => {
+            validateText(fixture, '.title', component.plan.title);
+        });
+
+        it('shows the plan copy/markup', () => {
+            validateText(fixture, '.plan-copy div', 'SILVER TIER');
+            validateText(fixture, '.plan-copy div', 'Get the good stuff without the extra fancy fluff.');
+        });
+
+        it('shows the plan signup action button', () => {
+            validateText(fixture, '.actions a', component.plan.signupActionItem.title);
+        });
+
+        it('shows the plan signup action button icon', () => {
+            validateExist(fixture, '.actions a app-icon');
+        });
+    });
 });
