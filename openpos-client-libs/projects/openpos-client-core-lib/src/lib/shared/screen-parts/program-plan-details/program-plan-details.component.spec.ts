fe86490e426f10fde55971c047f90b9603bf36f0
Robert Cochran <rcochran@manifestcorp.com>
2021-07-14 11:43:48 -0400

    test additions/tweaks

--- openpos-client-libs/projects/openpos-client-core-lib/src/lib/shared/screen-parts/program-plan-details/program-plan-details.component.spec.ts
+++ openpos-client-libs/projects/openpos-client-core-lib/src/lib/shared/screen-parts/program-plan-details/program-plan-details.component.spec.ts
@@ -1,8 +1,8 @@
 import { TestBed, ComponentFixture } from '@angular/core/testing';
-import { NO_ERRORS_SCHEMA } from '@angular/core'
+import {NO_ERRORS_SCHEMA, SecurityContext} from '@angular/core'
 
 import {ActionService} from '../../../core/actions/action.service';
-import {validateText} from '../../../utilites/test-utils';
+import {validateExist, validateText} from '../../../utilites/test-utils';
 import {MatDialog} from '@angular/material';
 import {HttpClientTestingModule} from '@angular/common/http/testing';
 import {ElectronService} from 'ngx-electron';
@@ -13,7 +13,13 @@ import {KeyPressProvider} from "../../../shared/providers/keypress.provider";
 import {ProgramPlanDetailsComponent} from "./program-plan-details.component";
 import {Plan} from "../../../screens-with-parts/sale/program-interface";
 import {ActionItem} from "../../../core/actions/action-item";
-import {By} from "@angular/platform-browser";
+import {By, DomSanitizer} from "@angular/platform-browser";
+import {
+    SafeHtml, SafeResourceUrl,
+    SafeScript,
+    SafeStyle, SafeUrl,
+    SafeValue
+} from "@angular/platform-browser/src/security/dom_sanitization_service";
 
 class MockActionService {};
 class MockMatDialog {};
@@ -25,9 +31,36 @@ class MockKeyPressProvider {
     }
 };
 
+class MockDomSanitizer extends DomSanitizer {
+    bypassSecurityTrustHtml(copy: string): SafeHtml {
+        return "" as SafeHtml;
+    }
+
+    sanitize(context: SecurityContext, value: SafeValue | string | null): string | null {
+        return undefined;
+    }
+
+    bypassSecurityTrustStyle(value: string): SafeStyle {
+        return undefined;
+    }
+
+    bypassSecurityTrustScript(value: string): SafeScript {
+        return undefined;
+    }
+
+    bypassSecurityTrustUrl(value: string): SafeUrl {
+        return undefined;
+    }
+
+    bypassSecurityTrustResourceUrl(value: string): SafeResourceUrl {
+        return undefined;
+    }
+}
+
 describe('ProgramPlanDetailsComponent', () => {
     let component: ProgramPlanDetailsComponent;
     let fixture: ComponentFixture<ProgramPlanDetailsComponent>;
+    let domSanitizer: DomSanitizer;
 
     beforeEach( () => {
         TestBed.configureTestingModule({
@@ -41,7 +74,8 @@ describe('ProgramPlanDetailsComponent', () => {
                 { provide: ElectronService, useClass: MockElectronService },
                 { provide: KeyPressProvider, useClass: MockKeyPressProvider },
                 { provide: ClientContext, useValue: {}},
-                { provide: CLIENTCONTEXT, useClass: TimeZoneContext}
+                { provide: CLIENTCONTEXT, useClass: TimeZoneContext},
+                { provide: DomSanitizer, useClass: MockDomSanitizer}
             ],
             schemas: [
                 NO_ERRORS_SCHEMA,
@@ -49,7 +83,7 @@ describe('ProgramPlanDetailsComponent', () => {
         }).compileComponents();
         fixture = TestBed.createComponent(ProgramPlanDetailsComponent);
         component = fixture.componentInstance;
-        component.programCopy = "programCopy" as string;
+        component.programCopy = "<div styles='color: green'>Hello World</div>" as string;
         component.plans = [{
                 iconImageUrl: "iconImageUrl",
                 iconText: "iconText",
@@ -73,11 +107,34 @@ describe('ProgramPlanDetailsComponent', () => {
     });
 
     describe('component', () => {
+        describe('ngOnInit', () => {
+           it('initializes the safeProgramCopy by sanitizing the programCopy', () => {
+               let sanitizedDom = "banana" as SafeHtml;
+               component.safeProgramCopy = undefined;
+               component.programCopy = "taco";
+               domSanitizer = TestBed.get(DomSanitizer);
+               expect(domSanitizer).toBeDefined();
+               spyOn(domSanitizer, 'bypassSecurityTrustHtml').and.returnValue(sanitizedDom);
+
+               component.ngOnInit();
+
+               expect(domSanitizer.bypassSecurityTrustHtml).toHaveBeenCalledWith(component.programCopy);
+               expect(component.safeProgramCopy).toBe(sanitizedDom);
+           });
+        });
     });
 
     describe('template', function () {
-        it('should display any programCopy given', function () {
-            validateText(fixture, ".program div", component.programCopy as string);
+        it('should display any safeProgramCopy given', function () {
+            component.safeProgramCopy = 'dumb';
+            fixture.detectChanges();
+            validateExist(fixture, '.program div');
+        });
+
+        it('should display not display programCopy when there is no copy', function () {
+            component.safeProgramCopy = 'dumb';
+            fixture.detectChanges();
+            validateExist(fixture, '.program div');
         });
 
         it('should display any plans given', function () {
